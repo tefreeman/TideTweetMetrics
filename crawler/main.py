c0d48bbece4bd98c0d0528fe59fdc2cbc6b09ec8
Trevor <tefreeman@crimson.ua.edu>
2024-02-05 13:08:44 -0600

    Added

---
+++ crawler/main.py
@@ -0,0 +1,5 @@
+from scheduler import CrawlerScheduler
+
+test = CrawlerScheduler(["alabama_cs", "umichcse", "EECS_UTK", "OleMissCompSci", "gatech_scs", "LSUCSE", "UFCISE", "TulaneSSE", "MITEECS", "UAZScience", "CSatUSC"], 7)
+test.start()
+
