cb04a75aa28566e40075ed0b98c9ba2f01696fbf
Travis <travispnguyen@gmail.com>
2024-02-08 15:24:37 -0600

    Added some comments, started error checking in crawler

--- dev_setup_test.py
+++ dev_setup_test.py
@@ -4,13 +4,10 @@ from selenium.webdriver.common.by import By
 import time
 
 driver = webdriver.Chrome()
-driver.get("https://www.shitexpress.com/")
 
-elem = driver.find_element(By.ID, "preorder-toggle")
-driver.execute_script("window.scrollTo(0, 500)")
-time.sleep(1)
-elem.click()
-name = driver.find_element(By.ID, "order-name")
-name.send_keys("Trevor Freeman")
-time.sleep(5)
-driver.close()
+driver.get("https://nitter.soopy.moe/alabama_cs")
+elements = driver.find_elements(By.CLASS_NAME, "timeline-item")
+
+    
+
+time.sleep(20)
