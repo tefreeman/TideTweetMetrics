650740ec7780eb601d2b6036f0b97010dcdc52db
EJYohannan <99680407+EJYohannan@users.noreply.github.com>
2024-02-08 13:54:48 -0600

    Initial completion of database.py

--- crawler/database.py
+++ crawler/database.py
@@ -1,35 +1,105 @@
 from pymongo import MongoClient
+from typing import TypedDict
+from twitter_api_encoder import Tweet, Profile
+import time
+import datetime
 
-# from twitter_api_encoder import TweetEncoder, ProfileEncoder
-
-# ask for external ip
-
-client = MongoClient("mongodb://Admin:We420%3FZ4!@73.58.28.154:27017/")
-
+client = MongoClient("10.0.0.28", port=27017, username="Admin", password="We420?Z4!")
 db = client["twitter"]
 
-print("hello world")
+
+def attach_new_tweet_meta(obj: dict):
+    if "imeta" not in obj:
+        obj["imeta"] = {}
+
+    obj["imeta"]["created"] = datetime.datetime.now()
+    obj["imeta"]["update_id"] = (None,)
+    obj["imeta"]["errors"] = []
+    obj["imeta"]["version"] = 1
 
 
 def get_crawl_list() -> list[str]:
     collection = db["crawl_list"]
+    usernames = []
+    for user in collection.find():
+        if not ("username" in user):
+            raise Exception("username not inside user in crawl_list collection")
+
+        usernames.append(user["username"])
+
+    return usernames
 
 
 def get_crawl_history(acccount: str) -> list[str]:
-    pass
+    collection = db["crawl_history"]
 
 
-# def upsert_twitter_profile(profile: ProfileEncoder):
-#     collection = db["profiles"]
+def upsert_twitter_profile(profile: Profile, backup_file_id: int):
+    collection = db["profiles"]
+
+    if collection.find_one({"username": profile.get_username()}) != None:
+        return _update_profile(profile, backup_file_id)
+
+    collection.insert_one(profile.encode_as_dict(backup_file_id))
 
 
-# def upsert_tweets(tweets: list[TweetEncoder]):
-#     collection = db["tweets"]
+def _update_profile(profile: Profile, backup_file_id: int):
+    profile_updates_col = db["profile_updates"]
+    profile_col = db["profiles"]
+
+    result = profile_updates_col.insert_one(
+        profile.encode_changes_as_dict(backup_file_id)
+    )
+    profile_col.update_one(
+        {"username": profile.get_username()},
+        {"$set": {"imeta.update_id": result.inserted_id}},
+    )
+
+
+def upsert_tweets(tweets: list[Tweet], backup_file_id: int):
+    for tweet in tweets:
+        upsert_tweet(tweet, backup_file_id)
+
+
+def upsert_tweet(tweet: Tweet, backup_file_id):
+    collection = db["tweets"]
+    if collection.find_one({"data.id": tweet.get_id()}) != None:
+        return _update_tweet(tweet, backup_file_id)
+    else:
+        tweet_as_json = tweet.encode_as_dict(backup_file_id)
+        attach_new_tweet_meta(tweet_as_json)
+        collection.insert_one(tweet_as_json)
+
+
+def _update_tweet(tweet: Tweet, backup_file_id: int):
+    update_col = db["tweet_updates"]
+    tweet_col = db["tweets"]
+    result = update_col.insert_one(tweet.encode_changes_as_dict(backup_file_id))
+
+    tweet_col.update_one(
+        {"data.id": tweet.get_id()}, {"$set": {"imeta.update_id": result.inserted_id}}
+    )
 
 
 def get_mirrors() -> list[dict]:
     collection = db["mirrors"]
+    mirrors = []
+    for mirror in collection.find():
+        if not (
+            "url" in mirror
+            and "is_working" in mirror
+            and "up_events" in mirror
+            and "down_events" in mirror
+        ):
+            raise Exception(
+                "mirror, is_working, up_events, or down_events not inside mirror in mirrors collection"
+            )
+
+        mirrors.append(mirror)
+
+    return mirrors
 
 
 def save_mirror(mirror: dict):
     collection = db["mirrors"]
+    collection.replace_one({"url": mirror["url"]}, mirror, upsert=True)
