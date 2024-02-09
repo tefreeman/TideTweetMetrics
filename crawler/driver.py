b74c93ee04669e87b6f4ef0a51a57ce6e99ad56f
Trevor Freeman <tefreeman@crimson.ua.edu>
2024-02-09 17:47:04 -0600

    Changes to get crawler running

--- crawler/driver.py
+++ crawler/driver.py
@@ -15,8 +15,9 @@ def create_undetected_driver()-> webdriver.Chrome:
     capabilities['goog:loggingPrefs'] = {'performance': 'ALL'}
     #chromeOptions = webdriver.ChromeOptions()
     chrome_options = uc.ChromeOptions()
+    #driver = webdriver.Chrome()
     #driver = uc.Chrome(enable_cdp_events=True)
-    driver = uc.Chrome(options=chrome_options)
+    driver = uc.Chrome(options=chrome_options, desired_capabilities=capabilities)
     
     # additional method via monkey patching
     driver.get_status_code: Callable[[], Optional[int]] = lambda: _get_status_code(driver)
