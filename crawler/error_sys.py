39ecb83304501bb5a6c2f2557b79a3ab87060d6b
Travis <travispnguyen@gmail.com>
2024-02-10 15:49:04 -0600

    Updated error checking, added error checking system

---
+++ crawler/error_sys.py
@@ -0,0 +1,33 @@
+from selenium.common.exceptions import TimeoutException
+from enum import Enum
+
+class ErrorType(Enum):
+    PARSING_ERROR = 1
+    MIRROR_ERROR = 2
+    HTTP_ERROR = 3
+
+class Error:
+    _parsing_error_set = {"IncompleteException"}
+    _mirror_error_set = {"TimeoutException", "ErrorPanelFound"}
+    _http_error_set = {}
+
+    def __init__(self, error_name) -> None:
+        self.error_name = error_name
+        self.error_type = self._find_error_type()
+
+    def _find_error_type(self) -> ErrorType:
+        if self.error_name in Error._parsing_error_set:
+            return ErrorType.PARSING_ERROR
+        elif self.error_name in Error._mirror_error_set:
+            return ErrorType.MIRROR_ERROR
+        elif self.error_name in Error._http_error_set:
+            return ErrorType.HTTP_ERROR
+    
+    def get_error_type(self) -> ErrorType:
+        return self.error_type
+            
+
+try:
+    raise TimeoutException
+except TimeoutException as e:
+    Error(e.__class__.__name__)
