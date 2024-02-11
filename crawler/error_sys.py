58a3ca2950552a4cb042cd0bce86f69869177781
Trevor Freeman <tefreeman@crimson.ua.edu>
2024-02-11 11:13:05 -0600

    to_json added

--- crawler/error_sys.py
+++ crawler/error_sys.py
@@ -8,8 +8,8 @@ class ErrorType(Enum):
 
 class Error:
     _parsing_error_set = {"IncompleteException"}
-    _mirror_error_set = {"TimeoutException", "ErrorPanelFound"}
-    _http_error_set = {}
+    _mirror_error_set = {"TimeoutException", "ErrorPanelFound", "BadHTTPResponseCode"}
+    _internet_error_set = {}
 
     def __init__(self, error_name) -> None:
         self.error_name = error_name
@@ -20,12 +20,14 @@ class Error:
             return ErrorType.PARSING_ERROR
         elif self.error_name in Error._mirror_error_set:
             return ErrorType.MIRROR_ERROR
-        elif self.error_name in Error._http_error_set:
+        elif self.error_name in Error._internet_error_set:
             return ErrorType.HTTP_ERROR
     
     def get_error_type(self) -> ErrorType:
         return self.error_type
-            
+    
+    def to_json(self) -> dict:
+        return {"error_name": self.error_name, "error_type": self.error_type.name}   
 
 try:
     raise TimeoutException
