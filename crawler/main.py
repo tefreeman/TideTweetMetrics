4e2e4ec12de8a0aaf590c1103188858f47e2ffa0
Trevor Freeman <tefreeman@crimson.ua.edu>
2024-02-11 11:13:15 -0600

    config setup

--- crawler/main.py
+++ crawler/main.py
@@ -1,9 +1,12 @@
 from scheduler import CrawlerScheduler
 import database as db
+from config import Config
+
+Config.Load()
+db.init_database()
 
 accounts = db.get_crawl_list()
-
-test = CrawlerScheduler(accounts, 1)
+test = CrawlerScheduler(accounts, Config.crawler_threads())
 test.start()
 
  
