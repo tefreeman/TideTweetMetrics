fd698fdfe5f4b191927df69444bc5d9e8081008e
Trevor Freeman <tefreeman@crimson.ua.edu>
2024-02-11 11:12:17 -0600

    error system integration and code clean up

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -7,48 +7,64 @@ import database as db
 from driver import create_undetected_driver
 import time
 from selenium.common.exceptions import WebDriverException
-
+from error_sys import Error
 
 class CrawlResults(TypedDict):
     profile: Profile
     tweets: List[Tweet]
     raw_data: str | None
     next_url: str | None
-    error: bool
+    errors: list[Error]
     
     
 class Crawler:
     def __init__(self) -> None:
         self.driver = create_undetected_driver()
-
-    def crawl(self, url: str) -> CrawlResults:
-        results: CrawlResults = {"profile": None, "tweets": [], "raw_data": None, "next_url": None, "error": False}
-
-
-        #TODO: Add in error handling that will define different error types
+    
+    def load_and_check_errors(self, url, errors: list[Error]) -> None:
         try:
             self.driver_load_page(url)
 
         except WebDriverException as e:
-            results["error"] = True
-            return results
+            errors.append(Error(e.__class__.__name__))
+            return
 
-        if self.driver.get_status_code() != 200:
-            results["error"] = True
+        if self.driver.get_status_code() >= 300:
+            errors.append(Error("BadHTTPResponseCode"))
+            return
+        
+        if self.driver.has_connection() == False:
+            errors.append(Error("NoInternetConnection"))
+            return
+       
+        html_not_loaded_error = self.detected_html_not_loaded()
+        if html_not_loaded_error != None:
+            errors.append(html_not_loaded_error)
+            return
+        
+        
+    def crawl(self, url: str) -> CrawlResults:
+        results: CrawlResults = {"profile": None, "tweets": [], "raw_data": None, "next_url": None, "errors": []}
+
+        self.load_and_check_errors(url, results["errors"])
+        
+        if len(results["errors"]) > 0:
             return results
         
-        if self.detected_html_not_loaded():
-            results["error"] = True
-            return results
-
-
         results["raw_data"] = self.get_raw_data()
-        results["profile"] = self.parse_profile()
-        results["tweets"], results["next_url"], results["error"] = self.parse_tweets()       
+        results["profile"], profile_errors = self.parse_profile()
+        results["tweets"], results["next_url"], tweet_errors = self.parse_tweets()       
+        
+        
+        results["errors"] += profile_errors
+        results["errors"] += tweet_errors
+        
+        cleaned_errors = [item for item in results["errors"] if item is not None]
+        results["errors"] = cleaned_errors
         
         return results
 
-    def detected_html_not_loaded(self) -> bool:
+    def detected_html_not_loaded(self) -> Error | None:
         raise NotImplementedError()
     
     def driver_load_page(self, url: str):
