f94d7fabe8425555a3e858dbe72c6dc4021b2dd5
Trevor Freeman <tefreeman@crimson.ua.edu>
2024-02-14 12:11:44 -0600

    Support for new backup system

--- crawler_sys/utils/backup.py
+++ crawler_sys/utils/backup.py
@@ -1,16 +1,19 @@
 from datetime import datetime
 import threading
-import os
+import os, zipfile
 import queue
 import lzma
+import glob
+import shutil
+
 
 _backup_dir = "backup_raw_data"
 _file_lock = threading.Lock()
 
-
-def backup_raw_data(raw_source: str, username: str) -> str:
+def back_up_html_file(raw_source: str, username: str) -> int:
     
     _file_lock.acquire()
+       
     save_dir = _backup_dir + "/" + username
     
     if not os.path.exists(save_dir):
@@ -31,3 +34,27 @@ def backup_raw_data(raw_source: str, username: str) -> str:
 
     _file_lock.release()
     return file_id
+
+
+def next_zip_num() -> int:
+    return len(glob.glob1("backups/","*.zip")) + 1
+
+def compress_backups():
+    name = 'backup_raw_data/'
+    zip_file_name = str(next_zip_num()) + '.zip'
+    zip_name = name + zip_file_name
+
+    with zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_LZMA) as zip_ref:
+        for folder_name, subfolders, filenames in os.walk(name):
+            for filename in filenames:
+                file_path = os.path.join(folder_name, filename)
+                zip_ref.write(file_path, arcname=os.path.relpath(file_path, name))
+
+    zip_ref.close()
+    
+    os.rename(zip_name, 'backups/' + zip_file_name)
+
+
+def delete_raw_data():
+    shutil.rmtree(_backup_dir)
+                      
