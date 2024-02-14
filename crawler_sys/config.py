695b7064c639a429c1edc93e51255ce7dd74656d
Trevor Freeman <tefreeman@crimson.ua.edu>
2024-02-14 12:12:36 -0600

    added backup support

--- crawler_sys/config.py
+++ crawler_sys/config.py
@@ -1,12 +1,19 @@
 import json
-
+from utils.backup import next_zip_num
 
 class Config:
     __CONFIG = None
+    _next_backup_zip_id = None
+    
     
     def __new__(cls):
         raise TypeError('Static classes cannot be instantiated')
     
+    @staticmethod
+    def init() -> None:
+        Config.Load()
+        Config._next_backup_zip_id = next_zip_num()
+        
     @staticmethod
     def db_name() -> str:
         return Config.__CONFIG["DB"]["NAME"]
@@ -35,6 +42,10 @@ class Config:
     @staticmethod
     def get_version() -> str:
         return Config.__CONFIG["VERSION"]
+    
+    @staticmethod
+    def next_run_backup_zip_id() -> int:
+        return Config._next_backup_zip_id
     
     @staticmethod
     def Load():
