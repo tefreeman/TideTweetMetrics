b0b443c15f7f971c3df5848defa88f38072de3a4
EJYohannan <99680407+EJYohannan@users.noreply.github.com>
2024-02-15 12:42:23 -0600

    Initial tests for database.py; not yet complete

--- crawler_sys/database.py
+++ crawler_sys/database.py
@@ -10,25 +10,30 @@ from config import Config
 client = None
 db = None
 
+
 def init_database():
     global client, db
-    client = MongoClient(Config.db_host(), port=Config.db_port(), username=Config.db_user(), password=Config.db_password())
+    client = MongoClient(
+        Config.db_host(),
+        port=Config.db_port(),
+        username=Config.db_user(),
+        password=Config.db_password(),
+    )
     db = client[Config.db_name()]
-    
 
     collection_names = db.list_collection_names()
     if len(collection_names) == 0:
         _init_collections()
-        
-        
+
+
 def _init_collections():
     old_db = client["twitter"]
-    
+
     db.create_collection("crawl_list")
     crawl_list = old_db["crawl_list"].find({})
     for user in crawl_list:
         db["crawl_list"].insert_one(user)
-   
+
     db.create_collection("mirrors")
     mirror_list = old_db["mirrors"].find({})
     for mirror in mirror_list:
@@ -36,16 +41,31 @@ def _init_collections():
         mirror["down_events"] = 0
         mirror["is_working"] = True
         db["mirrors"].insert_one(mirror)
-   
+
     db.create_collection("profiles")
     db.create_collection("tweets")
-   
+
     db["tweets"].create_index([("data.id", 1)], unique=True)
     db["profiles"].create_index([("username", 1)], unique=True)
-   
-    db.create_collection("profile_updates", timeseries={"timeField": "timestamp", 'metaField': 'imeta', 'granularity': 'hours'})
-    db.create_collection("tweet_updates", timeseries={"timeField": "timestamp", 'metaField': 'imeta', 'granularity': 'hours'})
-    
+
+    db.create_collection(
+        "profile_updates",
+        timeseries={
+            "timeField": "timestamp",
+            "metaField": "imeta",
+            "granularity": "hours",
+        },
+    )
+    db.create_collection(
+        "tweet_updates",
+        timeseries={
+            "timeField": "timestamp",
+            "metaField": "imeta",
+            "granularity": "hours",
+        },
+    )
+
+
 def get_crawl_list() -> list[str]:
     collection = db["crawl_list"]
     usernames = set()
@@ -57,25 +77,30 @@ def get_crawl_list() -> list[str]:
 
     return list(usernames)
 
+
 def add_crawl_summary(summary: dict):
     collection = db["crawl_summaries"]
     collection.insert_one(summary)
-    
-    
+
+
 def get_crawl_history(acccount: str) -> list[str]:
     collection = db["crawl_history"]
 
-    
+
 def upsert_twitter_profile(profile: Profile):
     collection = db["profiles"]
     profile_updates_col = db["profile_updates"]
-    
+
     db_profile = collection.find_one({"username": profile.get_username()})
     if db_profile != None:
         if db_profile["imeta"]["uid"] != None:
-            update = profile_updates_col.find_one({"_id": ObjectId(db_profile["imeta"]["uid"])})
+            update = profile_updates_col.find_one(
+                {"_id": ObjectId(db_profile["imeta"]["uid"])}
+            )
             if update != None:
-                if update["timestamp"] > datetime.datetime.now() - datetime.timedelta(days=Config.get_profile_min_update_time()):
+                if update["timestamp"] > datetime.datetime.now() - datetime.timedelta(
+                    days=Config.get_profile_min_update_time()
+                ):
                     return None
         return _update_profile(profile)
 
@@ -88,10 +113,8 @@ def _update_profile(profile: Profile):
     profile_col = db["profiles"]
 
     profile.get_meta_ref().set_as_update(profile.get_username())
-    
-    result = profile_updates_col.insert_one(
-        profile.changes_to_json_dict()
-    )
+
+    result = profile_updates_col.insert_one(profile.changes_to_json_dict())
     profile_col.update_one(
         {"username": profile.get_username()},
         {"$set": {"imeta.uid": result.inserted_id}},
@@ -103,26 +126,27 @@ def upsert_tweets(tweets: list[Tweet]) -> list[ObjectId]:
     results = [upsert_tweet(tweet) for tweet in tweets]
     return results
 
+
 def upsert_tweet(tweet: Tweet):
     collection = db["tweets"]
     if collection.find_one({"data.id": tweet.get_id()}) != None:
         return _update_tweet(tweet).inserted_id
     else:
-        tweet.get_meta_ref().set_as_new()        
+        tweet.get_meta_ref().set_as_new()
         return collection.insert_one(tweet.to_json_dict()).inserted_id
 
 
 def _update_tweet(tweet: Tweet):
     update_col = db["tweet_updates"]
     tweet_col = db["tweets"]
-    
+
     tweet.get_meta_ref().set_as_update(tweet.get_id())
     result = update_col.insert_one(tweet.changes_to_json_dict())
 
     tweet_col.update_one(
         {"data.id": tweet.get_id()}, {"$set": {"imeta.uid": result.inserted_id}}
     )
-    
+
     return result
 
 
