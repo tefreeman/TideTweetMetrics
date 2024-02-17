9d4d0b169d8d37187bd5668d7a9f3c0e58fdae09
Sam Gaines <samuelmgaines@gmail.com>
2024-02-17 22:19:23 +0000

    Fixed test cases. A couple of bug fixes.

--- crawler_sys/tests/test_tweet_encoder.py
+++ crawler_sys/tests/test_tweet_encoder.py
@@ -8,16 +8,16 @@ class TestTweetEncoder(unittest.TestCase):
         self.tweet = Tweet()
 
     def test_id(self):
-        self.tweet.set_id("abc")
-        self.assertEqual(self.tweet.get_id(), "abc")
+        self.tweet.set_id("http://www.twitter.com/status/135abc")
+        self.assertEqual(self.tweet.get_id(), "135")
 
     def test_text(self):
         self.tweet.set_text("abc")
         self.assertEqual(self.tweet.get_text(), "abc")
 
     def test_post_date(self):
-        self.tweet.set_post_date("February 3, 2024 · 12:00 UTC")
-        self.assertEqual(self.tweet.get_post_date(), "February 3, 2024 · 12:00 UTC")
+        self.tweet.set_post_date("Feb 03, 2024 · 12:00 PM UTC")
+        self.assertEqual(self.tweet.get_post_date(), "Feb 03, 2024 · 12:00 PM UTC")
         self.assertEqual(self.tweet.get_post_date_as_epoch(), 1706961600)
 
     def test_author(self):
@@ -25,7 +25,7 @@ class TestTweetEncoder(unittest.TestCase):
         self.assertEqual(self.tweet.get_author(), "abc")
 
     def test_public_metrics(self):
-        self.tweet.set_public_metrics(20, 10, 50, 5)
+        self.tweet.set_public_metrics("20", "10", "50", "5")
         resp = self.tweet.get_public_metrics()
         self.assertEqual(resp['retweet_count'], 20)
         self.assertEqual(resp['reply_count'], 10)
@@ -46,9 +46,9 @@ class TestTweetEncoder(unittest.TestCase):
         self.assertEqual(len(resp["hashtags"]), 1)
         self.assertEqual(len(resp["cashtags"]), 1)
         self.assertEqual(len(resp["urls"]), 1)
-        self.assertEqual(resp["mentions"]["start"], 4)
-        self.assertEqual(resp["mentions"]["end"], 14)
-        self.assertEqual(resp["mentions"]["tag"], "samgaines")
+        self.assertEqual(resp["mentions"][0]["start"], 4)
+        self.assertEqual(resp["mentions"][0]["end"], 14)
+        self.assertEqual(resp["mentions"][0]["tag"], "samgaines")
 
     def test_attachments(self):
         photos = ["photo1", "photo2"]
@@ -62,9 +62,10 @@ class TestTweetEncoder(unittest.TestCase):
 
     def test_referenced_tweet(self):
         id = "1256"
-        type = "retweeted"
+        type = ReferencedTweetType.RETWEET
         self.tweet.set_refenced_tweet(id, type)
-        self.assertEqual(self.tweet.get_referenced_tweet)
+        self.assertEqual(self.tweet.get_referenced_tweet()["id"], id)
+        self.assertEqual(self.tweet.get_referenced_tweet()["type"], "retweeted")
 
 if __name__ == '__main__':
     unittest.main()
