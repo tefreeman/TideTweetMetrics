6a9ba5c9f6f9ca48b15fdbf6fe8bc255de76805e
EJYohannan <99680407+EJYohannan@users.noreply.github.com>
2024-02-19 11:08:35 -0600

    Tests (of database.py) for basic CRUD on Tweets

--- crawler_sys/tests/test_database.py
+++ crawler_sys/tests/test_database.py
@@ -6,9 +6,14 @@ from encoders.tweet_encoder import Tweet
 from encoders.meta_encoder import MetaData
 from config import Config
 
+_init_flag = False
+
 
 class TestDatabase(unittest.TestCase):
-    def test_init_database(self):
+    def test_0_init_database(self):
+        global _init_flag
+        if _init_flag == True:
+            return
         # See if a new database is created (if we have a correct number of collections)
         Config.init()
         Database.init_database("Test_DB")
@@ -19,9 +24,11 @@ class TestDatabase(unittest.TestCase):
             self.assertEqual(
                 len(collections), 9, "Database should contain 9 collections."
             )
+        _init_flag = True
 
     ## insert tweet
-    def test_insert_tweet(self):
+    def test_1_insert_tweet(self):
+        self.test_0_init_database()
         curMD = MetaData()
         curMD.set_errors([])
         curMD.set_as_new()
@@ -52,7 +59,29 @@ class TestDatabase(unittest.TestCase):
         dbTweet = Database.db["tweets"].find_one({"data.id": "1234567890"})
         self.assertEqual(dbTweet["data"]["text"], "This is a tweet")
 
-    ## delete tweet
+    ## test update
+    def test_2_update_tweet(self):
+        self.test_1_insert_tweet()
+
+    def test_3_find_delete(self):
+        tweet = Database.db["tweets"].find_one({"data.id": "1234567890"})
+        tweet_update = Database.db["tweet_updates"].find_one(
+            {"imeta.oid": "1234567890"}
+        )
+
+        self.assertNotEqual(tweet, None)
+        self.assertNotEqual(tweet_update, None)
+
+        Database.db["tweets"].delete_one({"_id": tweet["_id"]})
+        Database.db["tweet_updates"].delete_one({"_id": tweet_update["_id"]})
+
+        tweet = Database.db["tweets"].find_one({"data.id": "1234567890"})
+        tweet_update = Database.db["tweet_updates"].find_one(
+            {"imeta.oid": "1234567890"}
+        )
+
+        self.assertEqual(tweet, None)
+        self.assertEqual(tweet_update, None)
 
     ## insert profile
 
