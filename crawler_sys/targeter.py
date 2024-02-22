aee2740147f5bf976e8304cdfcac6b19df265054
Trevor <tefreeman@crimson.ua.edu>
2024-02-22 13:52:46 -0600

    init targeter

---
+++ crawler_sys/targeter.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import urllib
+
+class Targeter:
+    def __init__(self) -> None:
+        target_json = {}
+    
+    def save():
+        pass
+    
+    def load():
+        pass
+    
+    
+class TwitterTargeter:
+    pass
