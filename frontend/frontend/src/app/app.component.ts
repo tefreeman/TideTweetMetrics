9941aed7ced384d1eadfec1c62c9d0307cf51f02
Travis <travispnguyen@gmail.com>
2024-03-20 18:18:23 -0500

    Scrapped first approach, implemented angular materials

--- frontend/frontend/src/app/app.component.ts
+++ frontend/frontend/src/app/app.component.ts
@@ -1,23 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { ClientsComponent } from './components/clients/clients.component';
-import { PricingComponent } from './components/pricing/pricing.component';
-import { TestimonialsComponent } from './components/testimonials/testimonials.component';
-import { GalleryComponent } from './components/gallery/gallery.component';
-import { NavComponent } from './components/nav/nav.component';
-import { ServicesComponent } from './components/services/services.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, HeaderComponent, DashboardComponent, ClientsComponent, PricingComponent, TestimonialsComponent, 
-  GalleryComponent, NavComponent, ServicesComponent, FooterComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, DashboardComponent]
 })
 export class AppComponent {
   title = 'frontend';
-  names = ["Trevor", "BLASA", "ASJJAS"];
 }
