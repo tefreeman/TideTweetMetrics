e80bc23492d251d2f0bbcd60cc4d670d86858f3c
Travis <travispnguyen@gmail.com>
2024-03-20 15:57:37 -0500

    Implemented Components (Done with template)

--- frontend/frontend/src/app/app.component.ts
+++ frontend/frontend/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ServicesComponent } from './components/services/services.component';
+import { FooterComponent } from './components/footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, AboutComponent, ClientsComponent, PricingComponent, TestimonialsComponent, 
+  GalleryComponent, NavComponent, ServicesComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
