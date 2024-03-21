cb740d77c57be89a5a8de4a1281b149f977ddd5e
Sam Gaines <samuelmgaines@gmail.com>
2024-03-21 21:55:38 +0100

    index and accounts endpoints

--- api/metrics/views.py
+++ api/metrics/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 import json
 from pymongo import MongoClient
+from metrics.stats import *
 
 hostname = "73.58.28.154"
 port = 27017
@@ -13,4 +14,9 @@ client = MongoClient(hostname, port, username=username, password=password)
 db = client['twitter_v1']
 
 def index(request):
-    return HttpResponse("Index page")
+    return HttpResponse("Nothing here. Add \"/metric_name\" to the URL to get a metric.")
+
+def getAccounts(request):
+    return HttpResponse(json.dumps(get_crawl_list(db)),
+            content_type='application/json; charset=utf8')
+
