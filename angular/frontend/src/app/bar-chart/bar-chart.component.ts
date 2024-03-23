469fea7c86c991adb983949346f1e08ba62eb76a
Trevor <tefreeman@crimson.ua.edu>
2024-03-23 15:29:31 -0500

    front end fixed

---
+++ angular/frontend/src/app/bar-chart/bar-chart.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit, Input} from '@angular/core';
+import Chart from 'chart.js/auto';
+
+@Component({
+  selector: 'app-bar-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './bar-chart.component.html',
+  styleUrl: './bar-chart.component.scss'
+})
+
+export class BarChartComponent implements OnInit {
+  @Input() graphId = '';
+  constructor(){}
+  public chart: any;
+
+  ngOnInit() {
+    this.createChart();
+  }
+  
+  createChart(){
+    this.chart = new Chart(this.graphId, {
+      type: 'bar', //this denotes tha type of chart
+
+      data: {// values on X-Axis
+        labels: ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13',
+								 '2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ], 
+	       datasets: [
+          {
+            label: "Sales",
+            data: ['467','576', '572', '79', '92',
+								 '574', '573', '576'],
+            backgroundColor: 'blue'
+          },
+          {
+            label: "Profit",
+            data: ['542', '542', '536', '327', '17',
+									 '0.00', '538', '541'],
+            backgroundColor: 'limegreen'
+          }  
+        ]
+      },
+      options: {
+        aspectRatio:2.5
+      }
+      
+    });
+  }
+
+}
