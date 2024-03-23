65d35df868945ff89e95ca4797a0849e43ea9536
Travis <travispnguyen@gmail.com>
2024-03-23 18:58:41 -0500

    Frontend Stuff

--- angular/frontend/src/app/graph.service.ts
+++ angular/frontend/src/app/graph.service.ts
@@ -8,7 +8,6 @@ export interface IChartData {
 export interface IDataset {
   label: string;
   data: string[]; // Represents numbers as strings; consider using number[] if the data are always numeric
-  backgroundColor: string;
 }
 
 export interface IGraph {
@@ -35,16 +34,14 @@ export class GraphService {
           '2022-05-14', '2022-05-15', '2022-05-16', '2022-05-17'],
         datasets: [
           {
-            label: "Sales",
+            label: "UA",
             data: ['467', '576', '572', '79', '92',
               '574', '573', '576'],
-            backgroundColor: 'blue'
           },
           {
-            label: "Profit",
+            label: "Other Universities",
             data: ['542', '542', '536', '327', '17',
               '0.00', '538', '541'],
-            backgroundColor: 'limegreen'
           }
         ]
       }
@@ -52,22 +49,59 @@ export class GraphService {
     {
       name: "g2",
       id: "canvas2",
-      type: "bar",
+      type: "line",
       data: {
         labels: ['2022-05-10', '2022-05-11', '2022-05-12', '2022-05-13',
           '2022-05-14', '2022-05-15', '2022-05-16', '2022-05-17'],
         datasets: [
           {
-            label: "Sales",
+            label: "UA",
             data: ['467', '576', '572', '79', '92',
               '574', '573', '576'],
-            backgroundColor: 'blue'
           },
           {
-            label: "Profit",
+            label: "Other Universities",
             data: ['542', '542', '536', '327', '17',
               '0.00', '538', '541'],
-            backgroundColor: 'limegreen'
+          }
+        ]
+      }
+    },
+    {
+      name: "g3",
+      id: "canvas3",
+      type: "line",
+      data: {
+        labels: ['2022-05-10', '2022-05-11', '2022-05-12', '2022-05-13',
+          '2022-05-14', '2022-05-15', '2022-05-16', '2022-05-17'],
+        datasets: [
+          {
+            label: "UA",
+            data: ['467', '576', '572', '79', '92',
+              '574', '573', '576'],
+          },
+          {
+            label: "Other Universities",
+            data: ['542', '542', '536', '327', '17',
+              '0.00', '538', '541'],
+          }
+        ]
+      }
+    },
+    {
+      name: "g4",
+      id: "canvas4",
+      type: "bar",
+      data: {
+        labels: ['All Time', 'Last 3 Months', 'Last Week'],
+        datasets: [
+          {
+            label: "UA",
+            data: ['467', '576', '572'],
+          },
+          {
+            label: "Other Universities",
+            data: ['542', '542', '536'],
           }
         ]
       }
