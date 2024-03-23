11112b63a25de9464295f59cb13678369f2dabfc
Trevor <tefreeman@crimson.ua.edu>
2024-03-23 12:59:50 -0500

    update

--- crawler_sys/main.py
+++ crawler_sys/main.py
@@ -22,7 +22,40 @@ def crawl_job():
         )
         compress_backups()
         remove_backup_files()
-    accounts = db.get_crawl_list()
+    #accounts = db.get_crawl_list()
+    famous_accounts = [
+    "@BarackObama", "@realDonaldTrump", "@BillGates", "@elonmusk", "@Oprah",
+    "@ladygaga", "@Cristiano", "@KingJames", "@rihanna", "@jtimberlake",
+    "@taylorswift13", "@narendramodi", "@katyperry", "@TheEllenShow", "@KimKardashian",
+    "@selenagomez", "@justinbieber", "@ArianaGrande", "@ddlovato", "@shakira",
+    "@jimmyfallon", "@realDonaldTrump", "@BrunoMars", "@Billieeilish", "@iamsrk",
+    "@LeoDiCaprio", "@kanyewest", "@JeffBezos", "@neiltyson", "@jk_rowling",
+    "@Malala", "@StephenKing", "@TheRock", "@TomHanks", "@Beyonce",
+    "@britneyspears", "@ChrisEvans", "@RobertDowneyJr", "@EmmaWatson", "@priyankachopra",
+    "@vancityreynolds", "@Drake", "@kobebryant", "@GretaThunberg", "@JoeBiden",
+    "@KamalaHarris", "@michelleobama", "@HillaryClinton", "@BernieSanders", "@AOC",
+    "@djkhaled", "@KevinHart4real", "@JLo", "@Pink", "@pitbull",
+    "@SnoopDogg", "@KylieJenner", "@Schwarzenegger", "@MileyCyrus", "@ParisHilton",
+    "@LilTunechi", "@DrPhil", "@RyanSeacrest", "@50cent", "@Eminem",
+    "@WizKhalifa", "@davidguetta", "@edsheeran", "@MariahCarey", "@SimonCowell",
+    "@gordonramsay", "@SteveHarveyFM", "@tyrabanks", "@alexa_chung", "@GiGiHadid",
+    "@karliekloss", "@MirandaKerr", "@AdrianaLima", "@Tyson_Fury", "@usainbolt",
+    "@MichaelPhelps", "@Simone_Biles", "@serenawilliams", "@rogerfederer", "@RafaelNadal",
+    "@Cristiano", "@WayneRooney", "@KAKA", "@andresiniesta8", "@XabiAlonso",
+    "@Ibra_official", "@MesutOzil1088", "@3gerardpique", "@luis16suarez", "@paulpogba",
+    "@MoSalah", "@VirgilvDijk", "@lewy_official", "@DeBruyneKev", "@neymarjr",
+    ]
+    non_famous = ["@imcsears", "@CoachLeeMartin", "@TheHoopHerald", "@sabanfaux", "@TriumphLife360", "@UA_Athletics","@DrSlotsStakeUS",
+    "@TopSpeedLLC", "@sells_tom", "@DaDverhulst", "@BDoris92267478", "@GrantCardone", "@therealworld_ai", "@RealCandaceO", "@engineers_feed", "@premierleague",
+    "@FortniteGame", "@wtfyummi", "@joaocpp1997", "@HomestuckOdd", "@ManUtd", "@_JHokanson", "@Tam_Khan", "@crypoqback", "@iamdannex",
+    "@ReachMorpheuss", "@neuralink", "@chainlink", "@PrimeHydrate", "@MattWalshBlog", "litecoin", "@Uniswap", "@Nohelema", "@KimboFlight",
+    "@0xBEMichael", "@bummiearo", "@FulfillmentPet1", "@takeafootnote", "@alii3910", "@ChandlerHallow"]
+    
+    accounts: list[str] = non_famous + famous_accounts
+    
+    for account in accounts:
+        account.replace("@", "")
+        
     crawl_scheduler = CrawlerScheduler(accounts, Config.crawler_threads())
     summary_results = crawl_scheduler.start()
     db.add_crawl_summary(summary_results)
