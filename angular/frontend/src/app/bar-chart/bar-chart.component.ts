f5b1a4e1b2a1970adc6268ed92ab3787fa7afa10
Travis <travispnguyen@gmail.com>
2024-03-23 18:58:41 -0500

    Frontend Stuff

--- angular/frontend/src/app/bar-chart/bar-chart.component.ts
+++ angular/frontend/src/app/bar-chart/bar-chart.component.ts
@@ -32,19 +32,66 @@ export class BarChartComponent implements OnInit {
       this.createChart();
     }
   }
+  
+
   createChart(){
     this.chart = new Chart(this.graph.id, {
-      type: 'bar', //this denotes tha type of chart
+      type: 'bar', // this denotes the type of chart
 
-      data: {// values on X-Axis
+      data: {
         labels: this.graph.data.labels,
-	       datasets: this.graph.data.datasets
+        datasets: this.graph.data.datasets.map((dataset, index) => ({
+          ...dataset,
+          backgroundColor: this.getBackgroundColor(index),
+          borderWidth: 1,
+          borderRadius: 10
+        }))
       },
       options: {
-        aspectRatio:2.5
+        aspectRatio: 2.5,
+        plugins: {
+          legend: {
+            display: true,
+            position: 'top',
+            labels: {
+              color: 'white' // Change legend label color
+            }
+          },
+          title: {
+            display: true,
+            text: 'Likes',
+            color: 'white' // Change title color
+          }
+        },
+        scales: {
+          y: {
+            beginAtZero: true,
+            ticks: {
+              color: 'white' // Change y-axis ticks color
+            },
+            grid: {
+              display: false, // Remove grid lines from the y-axis
+            },
+
+          },
+          x: {
+            ticks: {
+              color: 'white' // Change x-axis ticks color
+            },
+            grid: {
+              display: false // Remove grid lines from the y-axis
+            }
+          }
+        }
       }
-      
     });
+}
+
+
+  // Function to get background color based on index
+  getBackgroundColor(index: number): string {
+    const colors = ['rgba(241,116,99,255)', 'rgba(180,218,225,255)', 'rgba(255, 206, 86, 0.5)'];
+    return colors[index % colors.length]; 
   }
 
 }
