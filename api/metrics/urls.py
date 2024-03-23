10198953375ae69bc940b677018a286dc7f5c653
Trevor <tefreeman@crimson.ua.edu>
2024-03-23 18:50:43 -0500

    update

--- api/metrics/urls.py
+++ api/metrics/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("accountlist", views.getAccountList, name="getAccountList"),
     path("accounts", views.getAccounts, name="getAccounts"),
-    path("accounts/<str:account_username>", views.getAccount, name="getAccount")
+    path("accounts/<str:account_username>", views.getAccount, name="getAccount"),
+    path("accounts_all", views.getAccountsAllAverage, name="getAccountsAllAverages"),
 ]
