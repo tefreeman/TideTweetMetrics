b17fc9576ae34d9f851ffaa4863350756b15e32f
Trevor <tefreeman@crimson.ua.edu>
2024-03-25 11:36:43 -0500

    classifier updates

--- angular/classifier/src/app/app.component.ts
+++ angular/classifier/src/app/app.component.ts
@@ -3,15 +3,20 @@ import { RouterOutlet } from '@angular/router';
 import { ClassifyService } from './classify.service';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatCardModule, MatButtonModule],
+  imports: [RouterOutlet, MatCardModule, MatButtonModule, FormsModule, MatFormFieldModule, MatInputModule, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'classifier';
+  public uid: string = "";
   public tweet = {data: {text: "", id: ""}}
   constructor(private classifyService: ClassifyService) {
     this.classifyService.get_random_tweet().then((data) => {
@@ -19,8 +24,16 @@ export class AppComponent {
     });
   }
   
+  set_uid(email :string) {
+    this.uid = email;
+  
+  }
   clicked(flag: string) {
-    this.classifyService.set_random_tweet(this.tweet.data.id, "1", flag).then((data) => {
+    if (this.uid == "") {
+      alert("Please enter your email address");
+      return;
+    }
+    this.classifyService.set_random_tweet(this.tweet.data.id, this.uid, flag).then((data) => {
       this.tweet=data;
     });
   }
