4efcc1f6cfc0fd819e3566542eecfa4cedb3966e
Sam Gaines <samuelmgaines@gmail.com>
2024-03-25 16:42:26 +0100

    API changes

--- api/metrics/urls.py
+++ api/metrics/urls.py
@@ -5,7 +5,11 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("accountlist", views.getAccountList, name="getAccountList"),
-    path("accounts", views.getAccounts, name="getAccounts"),
+    #path("accounts", views.getAccounts, name="getAccounts"),
     path("accounts/<str:account_username>", views.getAccount, name="getAccount"),
     path("accounts_all", views.getAccountsAllAverage, name="getAccountsAllAverages"),
+    path("test/<str:lookback_weeks>", views.test, name="test"),
+    path("post_length_metric/", views.getPostLengthMetric, name="getPostLengthMetric"),
+    path("get_random_tweet", views.get_random_tweet, name="get_random_tweet"),
+    path("set_random_tweet/<str:id>/<str:uid>/<str:flag>", views.set_random_tweet, name="set_random_tweet")
 ]
