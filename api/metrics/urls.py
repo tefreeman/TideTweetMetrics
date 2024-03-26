01bb8be469730d9f76588dfa7c007c4dc206a5f0
Trevor Freeman <tefreeman@crimson.ua.edu>
2024-03-26 11:30:24 -0500

    sprint 2 final

--- api/metrics/urls.py
+++ api/metrics/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path("test/<str:lookback_weeks>", views.test, name="test"),
     path("post_length_metric/", views.getPostLengthMetric, name="getPostLengthMetric"),
     path("get_random_tweet", views.get_random_tweet, name="get_random_tweet"),
-    path("set_random_tweet/<str:id>/<str:uid>/<str:flag>", views.set_random_tweet, name="set_random_tweet")
+    path("set_random_tweet/<str:id>/<str:uid>/<str:flag>", views.set_random_tweet, name="set_random_tweet"),
+    path("get_user_responses_count", views.get_user_responses_count, name="get_user_responses_count")
 ]
