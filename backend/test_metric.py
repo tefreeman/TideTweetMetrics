6dbfc495eab7d2b593a7a360f44d85d38b99bb2d
Trevor Freeman <tefreeman@crimson.ua.edu>
2024-03-29 19:54:40 -0500

    Updated to work with total metric generation system

--- backend/test_metric.py
+++ backend/test_metric.py
@@ -1,4 +1,5 @@
-from metrics.common.average_likes_metric import *
+from metrics.common.total import TotalMetric
+from metrics.common.average_likes_metric import AverageLikesMetric
 from metrics.metrics_compiler import StatMetricCompiler
 from config import Config
 
@@ -11,6 +12,11 @@ m_compile = StatMetricCompiler()
 # add your metric like this
 m_compile.AddMetric(AverageLikesMetric())
 
+# progrmatic way to add total on different properties
+m_compile.AddMetric(TotalMetric("Likes", "get_like_count"))
+m_compile.AddMetric(TotalMetric("Retweets", "get_retweet_count"))
+m_compile.AddMetric(TotalMetric("Replies", "get_reply_count"))
+m_compile.AddMetric(TotalMetric("Quotes", "get_quote_count"))
 
 test = m_compile.Process()
 print(test)
