b5cf12b1fe3d480c38cf95a0c53a2383cd8f725d
Trevor <tefreeman@crimson.ua.edu>
2024-04-03 12:21:59 -0500

    New attempt at metric system

--- backend/metrics/metrics.py
+++ backend/metrics/metrics.py
@@ -1,25 +1,27 @@
 from backend.encoders.tweet_encoder import Tweet
 from backend.encoders.profile_encoder import Profile
 from backend.encoders.metric_encoder import MetricEncoder
-from backend.metrics.tweet_property_profile_compiler import TweetPropertyProfileCompiler
+from backend.metrics.profile_stats.tweet_property_profile_compiler import TweetPropertyProfileCompiler
 
 class Metric:
-    def __init__(self, name: str,  update_over_tweets = False, update_over_profiles = False,):
+    def __init__(self, owner: str, name: str,  update_over_tweets = False, update_over_profiles = False,):
         self.metric_encoder: MetricEncoder = MetricEncoder()
+        self._owner = owner
+        self._name = name
         self._update_over_tweets = update_over_tweets
         self._update_over_profiles = update_over_profiles
-        self._name = name
         
             
     def get_name(self) -> str:
-        if not self._name:
-            raise Exception("Name has not been set")
         return self._name
+    
+    def get_owner(self) -> str:
+        return self._owner
     
     def update_by_tweet(self, tweet: Tweet):
         raise NotImplementedError("UpdateByTweet method must be implemented in derived classes.")
 
-    def update_by_profile(self, profile: Profile):
+    def update_over_profile(self, profile: Profile):
         raise NotImplementedError("UpdateByProfile method must be implemented in derived classes.")
     
     def tweet_filter(self, tweet: Tweet):
@@ -32,6 +34,9 @@ class Metric:
         # Must be implemented in derived classes
         raise NotImplementedError("FinalUpdate method must be implemented in derived classes.")
 
+    def get_data(self):
+        return self.metric_encoder.get_dataset()
+    
     def get_encoder(self):
         return self.metric_encoder
 
