28151b5305ea341203708ede58f2b106fae2e2d9
Sam Gaines <samuelmgaines@gmail.com>
2024-04-04 20:00:45 +0100

    basic email sending

---
+++ backend/email/email_report.py
@@ -0,0 +1,27 @@
+from backend.config import Config
+
+Config.init()
+key = Config.email_key()
+
+def send_report(addresses: list[str]) -> None:
+    # These imports must be done INSIDE of this function (to prevent circular import)
+    from sendgrid import SendGridAPIClient
+    from sendgrid.helpers.mail import Mail
+
+    message = Mail(
+        from_email='tidetweetmetrics@gmail.com',
+        to_emails=addresses,
+        subject='Testing testing 123',
+        html_content='<strong>RAHHHHHH</strong>')
+    try:
+        # sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        sg = SendGridAPIClient(key)
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(e.message)
+
+if __name__ == "__main__":
+    send_report(["samuelmgaines@gmail.com"])
