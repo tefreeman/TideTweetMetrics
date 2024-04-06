c1c4d6af1d5408a1d91bd20fbe79dbb48d1f986a
Trevor Freeman <tefreeman@crimson.ua.edu>
2024-04-05 23:29:14 -0500

    Integrated Firebase, login, signup, and form validation

---
+++ web/frontend/src/app/app-register/app-register.component.ts
@@ -0,0 +1,59 @@
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { FormControl, Validators, FormGroup, ValidationErrors, ValidatorFn, AbstractControl } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButton } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-app-register',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButton
+  ],
+  templateUrl: './app-register.component.html',
+  styleUrls: ['./app-register.component.scss']
+})
+export class AppRegisterComponent {
+  private _service = inject(AuthService);
+
+  constructor(private router: Router) {}
+  // Define a custom validator to ensure that the two password fields match
+  passwordsMatchValidator: ValidatorFn = (group: AbstractControl<any, any>): ValidationErrors | null => {
+    const password1 = group.get('password1')!.value;
+    const password2 = group.get('password2')!.value;
+    return password1 === password2 ? null : { notMatching: true };
+  }
+
+
+  signupForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password1: new FormControl('', [Validators.required, Validators.minLength(9)]),
+    password2: new FormControl('', [Validators.required, Validators.minLength(9)]),
+  }, { validators: this.passwordsMatchValidator });
+
+  errorMessage = '';
+
+
+  onSubmit(): void {
+    if (this.signupForm.valid) {
+      const { email, password1 } = this.signupForm.value;
+      this._service
+        .signup(email!, password1!)
+        .then(() => this.router.navigate(['/']))
+        .catch((error) => {
+          console.error('Login Failed', error);
+          this.errorMessage = 'User creation failed! Please try again.';
+        });
+    } else {
+      this.errorMessage = 'Please correct the errors in the form.';
+    }
+  }
+}
--- web/frontend/src/app/app.component.ts
+++ web/frontend/src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, DashboardComponent],
+  imports: [RouterOutlet, HeaderComponent, DashboardComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'frontend';
 }
