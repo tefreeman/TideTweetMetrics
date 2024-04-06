0e6ad6de351c71ea994c6184d7b7d880ac357fc6
Trevor <tefreeman@crimson.ua.edu>
2024-04-06 12:43:22 -0500

    added auth guard

--- web/frontend/src/app/app.routes.ts
+++ web/frontend/src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { AppRegisterComponent } from './app-register/app-register.component';
 import { MainViewComponent } from './main-view/main-view.component';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth.guard';
 
 export const routes: Routes = [
-  {path: 'dashboard', component: MainViewComponent},
+  {path: 'dashboard', component: MainViewComponent, canActivate: [AuthGuard]},
   {path: 'register', component: AppRegisterComponent},
   {path: 'login', component: LoginComponent},
 ];
