a346f96ad64282bc0648d4f8d2a2c46ea2c9b2ca
Trevor Freeman <tefreeman@crimson.ua.edu>
2024-04-06 14:54:41 -0500

    moved components and added support for getting json metrics

--- web/frontend/src/app/account/register/register.component.spec.ts
+++ web/frontend/src/app/account/register/register.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AppRegisterComponent } from './app-register.component';
+import { RegisterComponent } from './register.component';
 
 describe('AppRegisterComponent', () => {
-  let component: AppRegisterComponent;
-  let fixture: ComponentFixture<AppRegisterComponent>;
+  let component: RegisterComponent;
+  let fixture: ComponentFixture<RegisterComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppRegisterComponent]
+      imports: [RegisterComponent]
     })
     .compileComponents();
-    
-    fixture = TestBed.createComponent(AppRegisterComponent);
+
+    fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- web/frontend/src/app/app.config.ts
+++ web/frontend/src/app/app.config.ts
@@ -6,9 +6,29 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),
-     provideAnimationsAsync(),
-    importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"tidetweetmetrics-a047f","appId":"1:376861563223:web:50a529a1e2c3cb92d2c612","storageBucket":"tidetweetmetrics-a047f.appspot.com","apiKey":"AIzaSyBrxP5MKlLCgO7beiII3sIVb2nMsay5TSg","authDomain":"tidetweetmetrics-a047f.firebaseapp.com","messagingSenderId":"376861563223","measurementId":"G-MEND79FYZ2"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch()),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp({
+          projectId: 'tidetweetmetrics-a047f',
+          appId: '1:376861563223:web:50a529a1e2c3cb92d2c612',
+          storageBucket: 'tidetweetmetrics-a047f.appspot.com',
+          apiKey: 'AIzaSyBrxP5MKlLCgO7beiII3sIVb2nMsay5TSg',
+          authDomain: 'tidetweetmetrics-a047f.firebaseapp.com',
+          messagingSenderId: '376861563223',
+          measurementId: 'G-MEND79FYZ2',
+        })
+      )
+    ),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideStorage(() => getStorage())),
+  ],
 };
