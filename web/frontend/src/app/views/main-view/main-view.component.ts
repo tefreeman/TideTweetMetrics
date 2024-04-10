0c38c946992a53393f74238bbc9cb0694f4efe01
Travis Nguyen <tpnguyen6@crimson.ua.edu>
2024-04-10 08:08:45 -0500

    Added Sidenav, quick fix to color

--- web/frontend/src/app/views/main-view/main-view.component.ts
+++ web/frontend/src/app/views/main-view/main-view.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../shared/header/header.component";
 import { DashboardComponent } from "../../dashboard/dashboard.component";
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
     selector: 'app-main-view',
     standalone: true,
     templateUrl: './main-view.component.html',
     styleUrl: './main-view.component.scss',
-    imports: [HeaderComponent, DashboardComponent]
+    imports: [HeaderComponent, DashboardComponent, MatSidenavModule]
 })
 export class MainViewComponent {
 
