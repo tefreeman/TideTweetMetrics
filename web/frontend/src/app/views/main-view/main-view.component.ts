36b31afea953b2d06ff45726ce03783485831a18
Travis Nguyen <tpnguyen6@crimson.ua.edu>
2024-04-10 08:26:23 -0500

    Messing Around with buttons

--- web/frontend/src/app/views/main-view/main-view.component.ts
+++ web/frontend/src/app/views/main-view/main-view.component.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../../shared/header/header.component";
 import { DashboardComponent } from "../../dashboard/dashboard.component";
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
     selector: 'app-main-view',
     standalone: true,
     templateUrl: './main-view.component.html',
     styleUrl: './main-view.component.scss',
-    imports: [HeaderComponent, DashboardComponent, MatSidenavModule]
+    imports: [HeaderComponent, DashboardComponent, MatSidenavModule, MatButtonModule, MatTooltipModule, MatIconModule]
 })
 export class MainViewComponent {
 
