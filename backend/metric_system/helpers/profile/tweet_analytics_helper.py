5064e0304ce030cafe55d1cfb9fa00beeb50ba69
Trevor Freeman <tefreeman@crimson.ua.edu>
2024-04-11 12:58:28 -0500

    Added comments and refactored some code

--- backend/metric_system/helpers/profile/tweet_analytics_helper.py
+++ backend/metric_system/helpers/profile/tweet_analytics_helper.py
@@ -3,14 +3,17 @@ from pymongo import MongoClient
 from backend.encoders.tweet_encoder import Tweet
 from pymongo import ASCENDING
 from backend.metric_system.helpers.profile.profile_with_tweet_properties import ProfileWithTweetProperties
+from backend.metric_system.helpers.key_map import GLOBAL_PROFILE_NAME
 import logging
 
+
+# Helper class that gives access to tweet statistics for all profiles
 class TweetAnalyticsHelper:
-    def __init__(self, limit_for_debug=False) -> None:
-        self._limit_for_debug = limit_for_debug
-        self._has_built = False
+    def __init__(self, debug_mode=False) -> None:
+        self._is_debug_mode = debug_mode
+
         self._profile_store: dict[str, ProfileWithTweetProperties] = {}
-        self._tweets_col = self._connect_to_database()["tweets"]
+        self._tweets_collection = self._connect_to_database()["tweets"]
 
     def _connect_to_database(self):
         return MongoClient(
@@ -24,14 +27,14 @@ class TweetAnalyticsHelper:
         db = self._connect_to_database()
         return list(db["profiles"].find({}))
 
-
-        
-        
-    def _compute_stats_for_all_profiles(self):
+    def _init_global_profile(self):
         global_profile = ProfileWithTweetProperties()
         global_profile.set_public_metrics(0,0,0,0)
-        global_profile.set_username("_global")
-        
+        global_profile.set_username(GLOBAL_PROFILE_NAME) 
+        return global_profile
+    
+    def _compute_stats_for_all_profiles(self):
+        global_profile = self._init_global_profile()
         profiles = self._load_profiles()
         
         for profile in profiles:
@@ -39,36 +42,38 @@ class TweetAnalyticsHelper:
             global_profile.merge_public_metrics(profile_stat)
             
             tweets = None
-            if self._limit_for_debug:
+            if self._is_debug_mode:
                 logging.debug(f"Debug limit is set to True. Retrieving first 20 tweets from {profile_stat.get_username()} only")
                 tweets = list(
-                    self._tweets_col.find({"data.author_id": profile_stat.get_username()}).sort(
+                    self._tweets_collection.find({"data.author_id": profile_stat.get_username()}).sort(
                         "data.created_at", ASCENDING
                     ).limit(20)
                 )
             else:
                 logging.debug(f"Retrieving all tweets from {profile_stat.get_username()} by date ascending")
                 tweets = list(
-                    self._tweets_col.find({"data.author_id": profile_stat.get_username()}).sort(
+                    self._tweets_collection.find({"data.author_id": profile_stat.get_username()}).sort(
                         "data.created_at", ASCENDING
                     )
                 )
                 
             profile_tweets = [Tweet(as_json=tweet) for tweet in tweets]
-            
             profile_stat.build_stats(profile_tweets, is_sorted=True)
-            global_profile.build_stats(profile_tweets, is_sorted=False) # profiles are sorted but multiple profiles are not sorted with respect to each other            
+            
+            # profiles are sorted but multiple profiles are not sorted with respect to each other
+            global_profile.build_stats(profile_tweets, is_sorted=False) 
             
             self._profile_store[profile_stat.get_username()] = profile_stat
 
         self._profile_store[global_profile.get_username()] = global_profile
-        self._has_built = True
     
     def build(self):
         self._compute_stats_for_all_profiles()
         
+    # Returns a profile
     def get_profile(self, username: str) -> ProfileWithTweetProperties:
         return self._profile_store[username]   
     
+    # Returns all profiles
     def get_all_profiles(self) -> dict[str, ProfileWithTweetProperties]:
         return self._profile_store
