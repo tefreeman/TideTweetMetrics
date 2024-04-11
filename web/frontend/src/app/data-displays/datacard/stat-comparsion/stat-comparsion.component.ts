b2b8cc94ea434c299617bf3d698a910aa466e812
Trevor Freeman <tefreeman@crimson.ua.edu>
2024-04-10 19:17:10 -0500

    Refactor displayable-interface.ts to add new types and remove unused types

---
+++ web/frontend/src/app/data-displays/datacard/stat-comparsion/stat-comparsion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stat-comparsion',
+  templateUrl: './stat-comparsion.component.html',
+  styleUrls: ['./stat-comparsion.component.css']
+})
+export class StatComparsionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
