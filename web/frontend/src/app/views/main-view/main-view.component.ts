7bbddd37a5d612df94e8559c4f6604379936929b
Trevor Freeman <tefreeman@crimson.ua.edu>
2024-04-11 21:09:55 -0500

    added nav menu and fixed routing

--- web/frontend/src/app/views/main-view/main-view.component.ts
+++ web/frontend/src/app/views/main-view/main-view.component.ts
@@ -5,14 +5,25 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatButtonModule} from '@angular/material/button';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatNavList } from '@angular/material/list';
+import { NgFor } from '@angular/common';
+
 
 @Component({
     selector: 'app-main-view',
     standalone: true,
     templateUrl: './main-view.component.html',
     styleUrl: './main-view.component.scss',
-    imports: [HeaderComponent, DashboardComponent, MatSidenavModule, MatButtonModule, MatTooltipModule, MatIconModule]
+    imports: [HeaderComponent, RouterLink, DashboardComponent, MatToolbar, MatNavList, NgFor, MatSidenavModule, MatButtonModule, MatTooltipModule, MatIconModule, RouterOutlet, SideNavComponent]
 })
 export class MainViewComponent {
 
+    public isExpanded = false;
+
+    public toggleMenu() {
+      this.isExpanded = !this.isExpanded;
+    }
 }
