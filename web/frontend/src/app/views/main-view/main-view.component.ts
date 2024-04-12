ce8048743d485da444a4dbd1f6fcaa8e96ca94d0
Trevor Freeman <tefreeman@crimson.ua.edu>
2024-04-11 23:17:35 -0500

    Routing fix

--- web/frontend/src/app/views/main-view/main-view.component.ts
+++ web/frontend/src/app/views/main-view/main-view.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../../shared/header/header.component";
 import { DashboardComponent } from "../../dashboard/dashboard.component";
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { MaterialModule } from '../../material/material.module';
@@ -9,7 +8,7 @@ import { MaterialModule } from '../../material/material.module';
     standalone: true,
     templateUrl: './main-view.component.html',
     styleUrl: './main-view.component.scss',
-    imports: [MaterialModule, RouterLink, RouterOutlet, HeaderComponent, DashboardComponent]
+    imports: [MaterialModule, RouterLink, RouterOutlet, DashboardComponent]
 })
 export class MainViewComponent {
 
