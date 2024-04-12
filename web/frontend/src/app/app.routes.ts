d501de65ff718d8b7409ce3697a822df9e09d955
Trevor Freeman <tefreeman@crimson.ua.edu>
2024-04-11 21:09:55 -0500

    added nav menu and fixed routing

--- web/frontend/src/app/app.routes.ts
+++ web/frontend/src/app/app.routes.ts
@@ -4,10 +4,19 @@ import { MainViewComponent } from './views/main-view/main-view.component';
 import { LoginComponent } from './views/account/login/login.component';
 import { AuthGuard } from './core/guards/auth.guard';
 import { HomeComponent } from './views/home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'dashboard', component: MainViewComponent, canActivate: [AuthGuard]},
+  {path: 'dashboard', component: MainViewComponent, canActivate: [AuthGuard],
+    children: [{
+      path: 'home',
+      component: DashboardComponent,
+      canActivate: [AuthGuard],
+      outlet: 'dashboard'
+    
+    }]
+  },
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
 ];
