80550fcc07fb0285e21e1a24dc51f21a0264a89e
Trevor Freeman <tefreeman@crimson.ua.edu>
2024-04-12 10:40:49 -0500

    created mock data

---
+++ web/frontend/src/app/core/services/mock-data.service.ts
@@ -0,0 +1,33 @@
+import { inject, Injectable } from '@angular/core';
+import { I_DisplayableData } from '../interfaces/displayable-interface';
+
+
+let test_displayed_data: I_DisplayableData = {
+  stat_name: "tweet_likes-mean",
+  owners: ["alabama_cs"],
+  type: "stat-value",
+  values: [1]
+
+}
+
+let test_displayed_data2: I_DisplayableData = {
+  stat_name: "tweet_likes-sum",
+  owners: ["alabama_cs"],
+  type: "stat-trend",
+  values: [10]
+
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MockDataService {
+  constructor() { 
+  }
+
+  getMockData(): I_DisplayableData[] {
+    return [test_displayed_data, test_displayed_data2];
+  }
+
+
+}
