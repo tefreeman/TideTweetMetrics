949550f54f2ae8bee079b9e3e13023cf7cbf192b
Travis Nguyen <tpnguyen6@crimson.ua.edu>
2024-04-12 16:16:33 -0500

    Completed Stat-trend component

--- web/frontend/src/app/data-displays/datacard/stat-trend/stat-trend.component.ts
+++ web/frontend/src/app/data-displays/datacard/stat-trend/stat-trend.component.ts
@@ -3,31 +3,37 @@ import { KeyTranslatorService } from '../../../core/services/key-translator.serv
 import { I_DisplayableData } from '../../../core/interfaces/displayable-interface';
 import { NgIf } from '@angular/common';
 import { MaterialModule } from '../../../core/modules/material/material.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-stat-trend',
   standalone: true,
-  imports: [MaterialModule, NgIf],
+  imports: [MaterialModule, NgIf, CommonModule],
   templateUrl: './stat-trend.component.html',
-  styleUrls: ['./stat-trend.component.css'],
+  styleUrls: ['./stat-trend.component.scss'],
 })
 export class StatTrendComponent implements OnInit {
   @Input({required: true}) displayableData!: I_DisplayableData;
 
-  private KeyTranslatorService: KeyTranslatorService = inject(KeyTranslatorService);
+  private keyTranslatorService: KeyTranslatorService = inject(KeyTranslatorService);
 
   metricName = "Tweet Likes";
-  lastPoint: number = 29;
-  nowPoint: number = 35;
+  lastPoint: number = 10;
+  nowPoint: number = 20;
   
   lastTime: number = 2018;
   nowTime: number = 2019;
 
+  difference: number = this.nowPoint - this.lastPoint;
+
   timeUnitString: string = "Years";
 
-  constructor() { }
+  constructor() {
+    
+   }
 
   ngOnInit() {
+    this.metricName = this.keyTranslatorService.translateKey(this.displayableData['stat_name']);
 
 
   }
