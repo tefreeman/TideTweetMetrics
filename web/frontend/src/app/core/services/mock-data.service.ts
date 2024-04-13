50e165d80886e7fdbb7668951323ea0f7805601b
Travis Nguyen <tpnguyen6@crimson.ua.edu>
2024-04-13 08:55:42 -0500

    styles

--- web/frontend/src/app/core/services/mock-data.service.ts
+++ web/frontend/src/app/core/services/mock-data.service.ts
@@ -58,6 +58,19 @@ let test_displayed_data8: I_DisplayableData = {
   type: "graph-bar",
   values: [10]
 }
+let test_displayed_data9: I_DisplayableData = {
+  stat_name: "tweet_likes-sum",
+  owners: ["alabama_cs"],
+  type: "stat-comp",
+  values: [10]
+}
+
+let test_displayed_data10: I_DisplayableData = {
+  stat_name: "tweet_likes-sum",
+  owners: ["alabama_cs"],
+  type: "graph-bar",
+  values: [10]
+}
 
 @Injectable({
   providedIn: 'root'
@@ -69,7 +82,7 @@ export class MockDataService {
 
 
   getMockData(): I_DisplayableData[] {
-    return [test_displayed_data, test_displayed_data2, test_displayed_data3, test_displayed_data4, test_displayed_data5, test_displayed_data6, test_displayed_data7, test_displayed_data8];
+    return [test_displayed_data, test_displayed_data2, test_displayed_data3, test_displayed_data4, test_displayed_data5, test_displayed_data6, test_displayed_data7, test_displayed_data8, test_displayed_data9, test_displayed_data10];
   }
 
 
