1f651c531b1754feedd765fc2003ee0b656041a6
Trevor <tefreeman@crimson.ua.edu>
2024-04-12 23:24:24 -0500

    hooked up graphs to generation framework

--- web/frontend/src/app/data-displays/graph-card/graph-card.component.ts
+++ web/frontend/src/app/data-displays/graph-card/graph-card.component.ts
@@ -2,10 +2,11 @@ import { Component, Input, OnInit } from '@angular/core';
 import { I_DisplayableData } from '../../core/interfaces/displayable-interface';
 import { MaterialModule } from '../../core/modules/material/material.module';
 import { NgIf } from '@angular/common';
-
+import { LineChartComponent } from './line-chart/line-chart.component';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
 @Component({
   standalone: true,
-  imports: [MaterialModule, NgIf],
+  imports: [MaterialModule, NgIf,BarChartComponent, LineChartComponent],
   selector: 'app-graph-card',
   templateUrl: './graph-card.component.html',
   styleUrls: ['./graph-card.component.scss']
