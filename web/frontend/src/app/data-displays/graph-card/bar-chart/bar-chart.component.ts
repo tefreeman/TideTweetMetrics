9d57ed2d2a8fd945771e1e6dbca0caa25fe8192e
Travis Nguyen <tpnguyen6@crimson.ua.edu>
2024-04-13 10:11:27 -0500

    Updated color pallete

--- web/frontend/src/app/data-displays/graph-card/bar-chart/bar-chart.component.ts
+++ web/frontend/src/app/data-displays/graph-card/bar-chart/bar-chart.component.ts
@@ -16,8 +16,7 @@ export class BarChartComponent {
     const myTheme: AgChartTheme = {
       baseTheme: 'ag-default',
       palette: {
-          fills: ['#a51e3', '#0076C5', '#21B372', '#FDDE02', '#F76700', '#D30018'],
-          strokes: ['black'],
+          fills: ['#a51e36', '#ff7f7f', '#ffa07a', '#ffd700', '#9acd32', '#87ceeb', '#6a5acd', '#9370db', '#8a2be2', '#00ced1', '#32cd32']
       },
       overrides: {
         common: {
@@ -39,16 +38,29 @@ export class BarChartComponent {
         text: "Title",
       },
       data: [
-        { month: 'Jan', avgTemp: 2.3, iceCreamSales: 162000 },
-        { month: 'Mar', avgTemp: 6.3, iceCreamSales: 302000 },
-        { month: 'May', avgTemp: 16.2, iceCreamSales: 800000 },
-        { month: 'Jul', avgTemp: 22.8, iceCreamSales: 1254000 },
-        { month: 'Sep', avgTemp: 14.5, iceCreamSales: 950000 },
-        { month: 'Nov', avgTemp: 8.9, iceCreamSales: 200000 },
-      ],
-      // Series: Defines which chart type and data to use
-      series: [{ type: 'bar', xKey: 'month', yKey: 'iceCreamSales' }],
-    }
+        { month: 'Mar', 1: 9000, 2: 10000, 3: 30000, 4: 60000, 5: 90000, 6: 120000, 7: 30000, 8: 40000, 9: 50000, 10: 60000, 11: 70000 },
+        { month: 'May', 1: 20000, 2: 30000, 3: 40000, 4: 70000, 5: 100000, 6: 130000, 7: 40000, 8: 50000, 9: 60000, 10: 70000, 11: 80000 },
+        { month: 'Jul', 1: 30000, 2: 40000, 3: 50000, 4: 80000, 5: 110000, 6: 140000, 7: 50000, 8: 60000, 9: 70000, 10: 80000, 11: 90000 },
+        { month: 'Sep', 1: 40000, 2: 50000, 3: 60000, 4: 90000, 5: 120000, 6: 150000, 7: 60000, 8: 70000, 9: 80000, 10: 90000, 11: 100000 },
+        { month: 'Nov', 1: 50000, 2: 60000, 3: 70000, 4: 100000, 5: 130000, 6: 160000, 7: 70000, 8: 80000, 9: 90000, 10: 100000, 11: 110000 },
+        { month: 'Jan', 1: 9000, 2: 10000, 3: 20000, 4: 50000, 5: 80000, 6: 100000, 7: 20000, 8: 30000, 9: 40000, 10: 50000, 11: 60000 },
+    ],
+    
+    // Series: Defines which chart type and data to use
+    series: [
+        { type: 'bar', xKey: 'month', yKey: '1' },
+        { type: 'bar', xKey: 'month', yKey: '2' },
+        { type: 'bar', xKey: 'month', yKey: '3' },
+        { type: 'bar', xKey: 'month', yKey: '4' },
+        { type: 'bar', xKey: 'month', yKey: '5' },
+        { type: 'bar', xKey: 'month', yKey: '6' },
+        { type: 'bar', xKey: 'month', yKey: '7' },
+        { type: 'bar', xKey: 'month', yKey: '8' },
+        { type: 'bar', xKey: 'month', yKey: '9' },
+        { type: 'bar', xKey: 'month', yKey: '10' }, // new series 10
+        { type: 'bar', xKey: 'month', yKey: '11' }, // new series 11
+    ],
   }
 
+}
 }
