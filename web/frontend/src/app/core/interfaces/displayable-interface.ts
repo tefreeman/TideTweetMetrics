46f836c9a17405f4f85e7fea728d7bb3589b10a7
Trevor Freeman <tefreeman@crimson.ua.edu>
2024-04-14 23:07:40 -0500

    integrated new graph maker service

--- web/frontend/src/app/core/interfaces/displayable-interface.ts
+++ web/frontend/src/app/core/interfaces/displayable-interface.ts
@@ -61,4 +61,6 @@ export interface I_GraphLineData {
   owners: Array<string>;
 }
 
-export type  T_DisplayableDataType = I_StatValueData | I_StatTrendData | I_StatCompData | I_GraphBarData | I_GraphLineData;
+export type T_DisplayableStat = I_StatValueData | I_StatTrendData | I_StatCompData;
+export type T_DisplayableGraph = I_GraphBarData | I_GraphLineData;
+export type  T_DisplayableDataType = T_DisplayableStat | T_DisplayableGraph;
