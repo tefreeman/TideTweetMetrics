8d32f4e1138e8af1e3cd9aa9b7f808f31a753aca
Travis Nguyen <tpnguyen6@crimson.ua.edu>
2024-04-15 15:30:23 -0500

    Add approved accounts list

--- web/frontend/src/app/views/main-view/my-profile/my-profile.component.ts
+++ web/frontend/src/app/views/main-view/my-profile/my-profile.component.ts
@@ -3,15 +3,17 @@ import { MaterialModule } from '../../../core/modules/material/material.module';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
+import {MatExpansionModule} from '@angular/material/expansion';
+
 @Component({
   standalone: true,
-  imports: [MaterialModule, FormsModule, MatFormFieldModule, MatInputModule],
+  imports: [MaterialModule, FormsModule, MatFormFieldModule, MatInputModule, MatExpansionModule],
   selector: 'app-my-profile',
   templateUrl: './my-profile.component.html',
   styleUrls: ['./my-profile.component.scss']
 })
 export class MyProfileComponent implements OnInit {
-
+  panelOpenState = false;
   constructor() { }
 
   ngOnInit() {
