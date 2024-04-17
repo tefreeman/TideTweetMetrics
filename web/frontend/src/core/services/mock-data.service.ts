d59b224ae7f624cc32ce854c41ede414efefdbe4
Travis Nguyen <tpnguyen6@crimson.ua.edu>
2024-04-17 13:03:20 -0500

    style fixes

--- web/frontend/src/core/services/mock-data.service.ts
+++ web/frontend/src/core/services/mock-data.service.ts
@@ -14,68 +14,23 @@ let test_displayed_data1: I_DisplayableRequest = {
   type: "stat-comp",
 }
 
-
-let test_displayed_data2: I_DisplayableRequest = {
-  stat_name: "tweet_likes-mean",
-  ownersConfig: {type: "specific", owners: ["alabama_cs"]},
-  type: "stat-value",
-}
-
-
-let test_displayed_data3: I_DisplayableRequest = {
-  stat_name: "tweet_likes-mean",
-  ownersConfig: {type: "specific", owners: ["alabama_cs"]},
-  type: "stat-value",
-}
-
 let test_displayed_data4: I_DisplayableRequest = {
   stat_name: "tweet_likes-mean",
-  ownersConfig: {type: "specific", owners: ["alabama_cs"]},
-  type: "stat-value",
+  ownersConfig: {type: "specific", owners: ["alabama_cs", "msu_egr"]},
+  type: "stat-comp",
 }
 
 
-let test_displayed_data5: I_DisplayableRequest = {
-  stat_name: "tweet_likes-mean",
-  ownersConfig: {type: "specific", owners: ["alabama_cs"]},
-  type: "stat-value",
-}
 
 
-let test_displayed_data6: I_DisplayableRequest = {
-  stat_name: "tweet_likes-mean",
-  ownersConfig: {type: "specific", owners: ["alabama_cs"]},
-  type: "stat-value",
-}
-
-
-let test_displayed_data7: I_DisplayableRequest = {
-  stat_name: "tweet_likes-mean",
-  ownersConfig: {type: "specific", owners: ["alabama_cs"]},
-  type: "stat-value",
-}
-
-let test_displayed_data8: I_DisplayableRequest = {
-  stat_name: "tweet_likes-mean",
-  ownersConfig: {type: "specific", owners: ["alabama_cs"]},
-  type: "stat-value",
-}
-
-let test_displayed_data9: I_DisplayableRequest = {
-  stat_name: "tweet_likes-mean",
-  ownersConfig: {type: "specific", owners: ["alabama_cs"]},
-  type: "stat-value",
-}
-
-
-let test_displayed_data10: I_DisplayableRequest = {
+let test_displayed_data2: I_DisplayableRequest = {
   stat_name: "tweet_likes-mean",
   ownersConfig: {type: "top", owners: ["alabama_cs"], count: 25},
   type: "graph-bar",
 }
 
 
-let test_displayed_data11: I_DisplayableRequest = {
+let test_displayed_data3: I_DisplayableRequest = {
   stat_name: "tweet_likes-mean",
   ownersConfig: {type: "all", owners: ["alabama_cs"], count: 5},
   type: "graph-line",
@@ -91,7 +46,7 @@ export class MockDataService {
 
 
   getMockData(): I_DisplayableRequest[] {
-    return [test_displayed_data, test_displayed_data1, test_displayed_data2, test_displayed_data3, test_displayed_data4, test_displayed_data5, test_displayed_data6, test_displayed_data7, test_displayed_data8, test_displayed_data9, test_displayed_data10, test_displayed_data11];
+    return [test_displayed_data, test_displayed_data1, test_displayed_data2, test_displayed_data3,test_displayed_data4];
   }
 
 
