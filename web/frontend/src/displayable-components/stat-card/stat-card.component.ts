c6f278fe81750fe8248a1bcad9d9f80c1c6d9c3b
Trevor Freeman <tefreeman@crimson.ua.edu>
2024-04-17 01:53:26 -0500

    2am commit you know there's a lot

--- web/frontend/src/displayable-components/stat-card/stat-card.component.ts
+++ web/frontend/src/displayable-components/stat-card/stat-card.component.ts
@@ -1,23 +1,29 @@
 import { Component, inject, Input, OnInit } from '@angular/core';
-import { NgIf, NgSwitch } from '@angular/common';
+import { AsyncPipe, NgClass, NgIf, NgSwitch } from '@angular/common';
 import { StatTrendComponent } from '../stat-trend/stat-trend.component';
 import { StatValueComponent } from '../stat-value/stat-value.component';
 import { StatCompComponent } from '../stat-comp/stat-comp.component';
 import {CdkDrag} from '@angular/cdk/drag-drop';
 import { MaterialModule } from '../../core/modules/material/material.module';
 import { T_DisplayableDataType, I_StatValueData, I_StatTrendData, I_StatCompData } from '../../core/interfaces/displayable-interface';
+import { EditModeService } from '../../core/services/edit-mode.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-stat-card',
   standalone: true,
-  imports: [StatValueComponent, NgIf, StatTrendComponent, MaterialModule, StatCompComponent, NgSwitch, CdkDrag],
+  imports: [StatValueComponent, NgIf, StatTrendComponent, MaterialModule, StatCompComponent, NgSwitch, CdkDrag, NgClass, AsyncPipe],
   templateUrl: './stat-card.component.html',
   styleUrl: './stat-card.component.scss'
 })
 export class StatCardComponent implements OnInit {
+  editModeService: EditModeService = inject(EditModeService);
+
   @Input({required: true}) displayableData!: T_DisplayableDataType;
   @Input({required: true}) placeHolderForEdit!: boolean;
 
+  editMode: Observable<boolean> = this.editModeService.getEditMode();
+  applyClass = true; 
   
   constructor() {
   }
@@ -34,6 +40,14 @@ export class StatCardComponent implements OnInit {
 
   isStatComp(data: T_DisplayableDataType): data is I_StatCompData {
     return data.type === "stat-comp";
+  }
+
+  onMouseOver(): void {
+    this.applyClass = false; 
+  }
+
+  onMouseLeave(): void {
+    this.applyClass = true; 
   }
 
 }
