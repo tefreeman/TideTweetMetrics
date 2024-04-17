9f8993aef2a6fc75130b76b58d7b4f8e068c4ea0
Travis Nguyen <tpnguyen6@crimson.ua.edu>
2024-04-17 14:00:08 -0500

    Added logout button to unauthorized page

--- web/frontend/src/views/unregistered/unauthorized/unauthorized.component.ts
+++ web/frontend/src/views/unregistered/unauthorized/unauthorized.component.ts
@@ -1,14 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
     selector: 'app-unauthorized',
     standalone: true,
     imports: [
-        CommonModule,
+        CommonModule
     ],
-    template: `<p>Please ask the admin to authorize you</p>`,
-    styleUrl: './unauthorized.component.scss',
+    templateUrl: './unauthorized.component.html',
+    styleUrls: ['./unauthorized.component.scss'],  // Note: Corrected 'styleUrl' to 'styleUrls' which should be an array
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UnauthorizedComponent { }
+export class UnauthorizedComponent {
+    constructor(private authService: AuthService) {}
+
+    signout() {
+        this.authService.signout();
+    }
+}
