6dfef3f13cfe48015b6b0b074f15f186b2a14675
Trevor Freeman <tefreeman@crimson.ua.edu>
2024-04-17 01:53:26 -0500

    2am commit you know there's a lot

--- web/frontend/src/views/registered/dashboard/dashboard.component.ts
+++ web/frontend/src/views/registered/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
 import { NgFor, NgIf,} from '@angular/common';
-import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, HostListener, NgZone, OnDestroy, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { inject } from '@angular/core';
-import { Observable, Subscription,} from 'rxjs';
+import { Observable, Subject, Subscription, debounceTime, fromEvent, takeUntil,} from 'rxjs';
 import { IDisplayableStats, I_DisplayableRequest, T_DisplayableDataType } from '../../../core/interfaces/displayable-interface';
 import { MetricService } from '../../../core/services/metric.service';
 import { DisplayableProviderService } from '../../../core/services/displayable-provider.service';
@@ -17,44 +17,190 @@ import { MatDialog } from '@angular/material/dialog';
 import { AddGraphComponent } from '../../../displayable-components/add-graph/add-graph.component';
 import { AddCardComponent } from '../../../displayable-components/add-card/add-card.component';
 
+
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [NgFor, StatCardComponent, BarChartComponent, AsyncPipe, MaterialModule, GraphCardComponent, CommonModule, CdkDrag, CdkDropList, AsyncPipe],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
-  providers: [MetricService]
+  providers: [MetricService],
+
 })
-export class DashboardComponent implements OnInit, OnDestroy{
+export class DashboardComponent implements OnInit, OnDestroy, AfterViewInit {
+
+  @ViewChildren('statcard', {read: ElementRef}) statCards!: QueryList<any>;
+  
   _displayProcessor = inject(DisplayableProviderService);
   editModeService: EditModeService = inject(EditModeService);
-
-
-  displayableDataArr: T_DisplayableDataType[] = [];
+  displayableCardArr: T_DisplayableDataType[] = [];
+  displayableGraphArr: T_DisplayableDataType[] = [];
+  positionArr: {x: number, y: number}[] = []
   editMode: Observable<boolean> = this.editModeService.getEditMode();
   subscription: any;
+  private destroy$ = new Subject<void>();
 
-  constructor(public dialog: MatDialog){
+  private statsCardsChangesSub!: Subscription;
+  constructor(private cdr: ChangeDetectorRef, public dialog: MatDialog, private ngZone: NgZone){
 }
 
   ngOnInit() {
     this.subscription = this._displayProcessor.displayables$.subscribe((data) => {
-      console.log(data);
-      this.displayableDataArr = data;
+      this.displayableCardArr = [];
+      this.displayableGraphArr = [];
+      data.forEach((displayable) => {
+        if (this.isCard(displayable)) {
+          this.displayableCardArr.push(displayable);
+        } else if (this.isGraph(displayable)) {
+          this.displayableGraphArr.push(displayable);
+        }
+      });
     
-    })
+    });
+
+    this.ngZone.runOutsideAngular(() => {
+      // Create an observable that listens to the window resize event.
+      fromEvent(window, 'resize')
+        .pipe(
+          debounceTime(300), // Debounce time in ms
+          takeUntil(this.destroy$) 
+        )
+        .subscribe((event) => {
+          this.ngZone.run(() => {
+            this.onResize();
+          });
+        });
+    });
 
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+
+    if (this.statsCardsChangesSub) {
+      this.statsCardsChangesSub.unsubscribe();
+    }
+
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
+  ngAfterViewInit() {
+    // Initial subscription
+    this.statsCardsChangesSub = this.statCards.changes.subscribe((queryList: QueryList<ElementRef>) => {
+      this.setElementPositions(queryList);
+      
+      // keeping views in sync
+      this.cdr.detectChanges();
+    });
+
+    // Trigger for existing elements
+    if (this.statCards.length) {
+      this.statCards.notifyOnChanges();
+    }
+  }
   
-  // ISSUE:  The drop function needs to be modified to handle the CSS grid layout. MoveItemInArray is not sufficient.
+  onResize() {
+    this.setElementPositions(this.statCards);
+  }
+
+  private swapClosestElements(index: number, dropPos: { x: number, y: number }) {
+    if (index < 0 || index >= this.positionArr.length) {
+      return;
+    }
+  
+    // First, let's determine the grid capacity and current condition.
+    const itemsPerRow = this.determineItemsPerRow();
+    const totalRows = Math.ceil(this.positionArr.length / itemsPerRow);
+    const itemsInLastRow = this.positionArr.length % itemsPerRow || itemsPerRow;
+    const dropInLastRowOrBelow = this.isDropInLastRowOrBelow(dropPos, itemsPerRow, totalRows, itemsInLastRow);
+  
+    if (dropInLastRowOrBelow) {
+      this.moveToEnd(index);
+    } else {
+      this.performSwap(index, dropPos);
+    }
+  }
+  
+  private determineItemsPerRow(): number {
+
+    if (this.positionArr.length < 2) {
+      return this.positionArr.length;
+    }
+  
+  
+    const firstItemYPos = this.positionArr[0].y;
+    const indexOfSecondRow = this.positionArr.findIndex(pos => pos.y !== firstItemYPos);
+    
+    if (indexOfSecondRow === -1) {
+      return this.positionArr.length;
+    }
+
+    return indexOfSecondRow;
+  }
+  
+  private isDropInLastRowOrBelow(dropPos: { x: number, y: number }, itemsPerRow: number, totalRows: number, itemsInLastRow: number): boolean {
+    const lastItem = this.positionArr[this.positionArr.length - 1];
+    if (!lastItem) {
+      return false;
+    }
+  
+    const estimatedRowHeight = lastItem.y - (this.positionArr[0]?.y || 0) / (totalRows - 1 || 1);
+    const bottomOfGrid = lastItem.y + estimatedRowHeight;
+
+    if (dropPos.y > bottomOfGrid || (dropPos.y > lastItem.y && this.positionArr.length % itemsPerRow !== 0)) {
+      return true;
+    }
+  
+    return false;
+  }
+  
+  private moveToEnd(index: number) {
+    // Move the dragged item to the end if dropped in an extra space or below the grid.
+    const item = this.displayableCardArr.splice(index, 1)[0];
+    this.displayableCardArr.push(item);
+    
+    const position = this.positionArr.splice(index, 1)[0];
+    this.positionArr.push(position);
+  }
+  
+  private performSwap(index: number, dropPos: { x: number, y: number }) {
+    let closestDistance = Number.MAX_VALUE;
+    let closestIndex = -1;
+  
+    this.positionArr.forEach((pos, idx) => {
+      const distance = Math.sqrt((pos.x - dropPos.x) ** 2 + (pos.y - dropPos.y) ** 2);
+      if (distance < closestDistance && idx !== index) {
+        closestDistance = distance;
+        closestIndex = idx;
+      }
+    });
+  
+    if (closestIndex !== -1) {
+      [this.displayableCardArr[index], this.displayableCardArr[closestIndex]] =
+      [this.displayableCardArr[closestIndex], this.displayableCardArr[index]];
+  
+      [this.positionArr[index], this.positionArr[closestIndex]] =
+      [this.positionArr[closestIndex], this.positionArr[index]];
+    }
+  }
+  
+  
+  private setElementPositions(queryList: QueryList<ElementRef>) {
+    queryList.forEach((elemRef, index) => {
+      if (elemRef && elemRef.nativeElement) { 
+        const rect = elemRef.nativeElement.getBoundingClientRect();
+        // Calc the center points
+        const centerX = rect.left + rect.width / 2;
+        const centerY = rect.top + rect.height / 2;
+        this.positionArr[index] = { x: centerX, y: centerY };
+      }
+    });
+  }
+  // ISSUE:  solved
   drop(event: CdkDragDrop<T_DisplayableDataType[]>) {
     console.log(event);
-    moveItemInArray(this.displayableDataArr, event.previousIndex, event.currentIndex);
+    this.swapClosestElements(event.previousIndex, { x: event.dropPoint.x, y: event.dropPoint.y });
   }
 
   isCard(displayableData: T_DisplayableDataType): boolean {
@@ -72,11 +218,7 @@ export class DashboardComponent implements OnInit, OnDestroy{
   
   openGraphCardDialog() {
     
-    /**
-     * Fullscreen dialog with 15px margins
-     * 
-     * -> to make it work, wee need extra styling on dialog component below... 
-     */
+
     const dialogRef = this.dialog.open(AddGraphComponent, {
       height: "calc(100% - 60px)",
       width: "calc(100% - 60px)",
@@ -91,11 +233,7 @@ export class DashboardComponent implements OnInit, OnDestroy{
 
   openStatCardDialog() {
     
-    /**
-     * Fullscreen dialog with 15px margins
-     * 
-     * -> to make it work, wee need extra styling on dialog component below... 
-     */
+
     const dialogRef = this.dialog.open(AddCardComponent, {
       height: "calc(100% - 60px)",
       width: "calc(100% - 60px)",
