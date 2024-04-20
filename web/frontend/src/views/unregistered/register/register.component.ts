b071b10bc123512d6e0978a98a1799ddd14f3faa
Trevor Freeman <tefreeman@crimson.ua.edu>
2024-04-20 18:10:18 -0500

    name removals

--- web/frontend/src/views/unregistered/register/register.component.ts
+++ web/frontend/src/views/unregistered/register/register.component.ts
@@ -1,24 +1,23 @@
 import { Component, HostListener, inject } from '@angular/core';
-import { AuthService } from '../../../core/services/auth.service';
-import { FormControl, Validators, FormGroup, ValidationErrors, ValidatorFn, AbstractControl } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule, MatCardTitle } from '@angular/material/card';
-import { MatButton } from '@angular/material/button';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
-import { MatGridList, MatGridTile } from '@angular/material/grid-list';
 import { MaterialModule } from '../../../core/modules/material/material.module';
+import { AuthService } from '../../../core/services/auth.service';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [
-    FormsModule,
-    ReactiveFormsModule,
-    MaterialModule
-  ],
+  imports: [FormsModule, ReactiveFormsModule, MaterialModule],
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
   private _service = inject(AuthService);
@@ -37,21 +36,30 @@ export class RegisterComponent {
     this.cols = innerWidth < 600 ? 1 : 2;
   }
   // Define a custom validator to ensure that the two password fields match
-  passwordsMatchValidator: ValidatorFn = (group: AbstractControl<any, any>): ValidationErrors | null => {
+  passwordsMatchValidator: ValidatorFn = (
+    group: AbstractControl<any, any>
+  ): ValidationErrors | null => {
     const password1 = group.get('password1')!.value;
     const password2 = group.get('password2')!.value;
     return password1 === password2 ? null : { notMatching: true };
-  }
+  };
 
-
-  signupForm = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password1: new FormControl('', [Validators.required, Validators.minLength(9)]),
-    password2: new FormControl('', [Validators.required, Validators.minLength(9)]),
-  }, { validators: this.passwordsMatchValidator });
+  signupForm = new FormGroup(
+    {
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password1: new FormControl('', [
+        Validators.required,
+        Validators.minLength(9),
+      ]),
+      password2: new FormControl('', [
+        Validators.required,
+        Validators.minLength(9),
+      ]),
+    },
+    { validators: this.passwordsMatchValidator }
+  );
 
   errorMessage = '';
-
 
   onSubmit(): void {
     if (this.signupForm.valid) {
