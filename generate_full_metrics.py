fe269948b8573087d60fb296b47948cfd8b654ea
EJYohannan <99680407+EJYohannan@users.noreply.github.com>
2024-04-20 21:46:07 -0500

    improvements

--- generate_full_metrics.py
+++ generate_full_metrics.py
@@ -1,14 +1,23 @@
+import logging
 from backend.metric_system.build import build_metrics
 from backend.config import Config
 
+# Configure logging
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
 
 def main():
+    logging.info("Starting the metric system build process...")
+
     # Call the build_metric_system function to build the metric system
     build_metrics("full_metric_output.json")
-
-    # Perform other operations using the metric system
+    logging.info("Metrics built and output to full_metric_output.json")
 
 
 if __name__ == "__main__":
+    logging.info("Initializing configuration...")
     Config.init()
     main()
+    logging.info("SCRIPT EXECUTION COMPLETED.")
