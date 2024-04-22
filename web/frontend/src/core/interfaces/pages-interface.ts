decffef122301e8f2185a8f4e97cdfac695bbbf7
EJYohannan <99680407+EJYohannan@users.noreply.github.com>
2024-04-22 17:45:11 -0500

    Documentation: Core -guards -interfaces modules/materials

--- web/frontend/src/core/interfaces/pages-interface.ts
+++ web/frontend/src/core/interfaces/pages-interface.ts
@@ -1,19 +1,34 @@
+/**
+ * Represents a displayable request.
+ */
 import { I_DisplayableRequest } from './displayable-interface';
 
+/**
+ * Represents a grid request entry.
+ */
 export interface I_GridRequestEntry {
   displayables: I_DisplayableRequest[];
   type: 'graph' | 'stat';
   order: number;
 }
 
+/**
+ * Represents a grid request entry with a name.
+ */
 export interface I_GridRequestEntryWithName extends I_GridRequestEntry {
   name: string;
 }
 
+/**
+ * Represents a grid entry.
+ */
 export interface I_GridEntry {
   [gridName: string]: I_GridRequestEntry;
 }
 
+/**
+ * Represents a page map.
+ */
 export interface I_PageMap {
   [page: string]: I_GridEntry;
 }
