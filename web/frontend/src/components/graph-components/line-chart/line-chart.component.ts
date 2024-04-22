f75477c7c8db42e78bc1722c800e6138ca25673f
EJYohannan <99680407+EJYohannan@users.noreply.github.com>
2024-04-22 17:01:42 -0500

    Documentation: line-chart simple-graph

--- web/frontend/src/components/graph-components/line-chart/line-chart.component.ts
+++ web/frontend/src/components/graph-components/line-chart/line-chart.component.ts
@@ -6,6 +6,10 @@ import { AgChartOptions } from 'ag-charts-community';
 import { I_GraphLineData } from '../../../core/interfaces/displayable-data-interface';
 import { GraphMakerService } from '../../../core/services/graph-maker.service';
 
+/**
+ * Represents the LineChartComponent class.
+ * This component is responsible for displaying a line chart.
+ */
 @Component({
   standalone: true,
   imports: [CommonModule, RouterOutlet, AgChartsAngular],
@@ -14,14 +18,26 @@ import { GraphMakerService } from '../../../core/services/graph-maker.service';
   styleUrls: ['./line-chart.component.scss'],
 })
 export class LineChartComponent {
+  /**
+   * The input data for the line chart.
+   */
   @Input({ required: true }) displayableData!: I_GraphLineData;
 
+  /**
+   * The service used to create the line chart.
+   */
   private graphMakerService: GraphMakerService = inject(GraphMakerService);
-  // Chart Options
 
+  /**
+   * The chart options for the line chart.
+   */
   public chartOptions: AgChartOptions = {};
-  constructor() {}
 
+  constructor() { }
+
+  /**
+   * Initializes the component.
+   */
   ngOnInit(): void {
     this.chartOptions = this.graphMakerService.createLineChart(
       this.displayableData
