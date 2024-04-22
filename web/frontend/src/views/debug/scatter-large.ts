de9a1a236d46317c809b7d6c9fd26787e591a7e5
Travis Nguyen <tpnguyen6@crimson.ua.edu>
2024-04-22 11:51:46 -0500

    Added scatter plot design to debug

--- web/frontend/src/views/debug/scatter-large.ts
+++ web/frontend/src/views/debug/scatter-large.ts
@@ -3,658 +3,58 @@ import { AgChartOptions, AgChartTheme } from 'ag-charts-community';
 
 function getData() {
   return {
-    Asia: [
-      {
-        gdpPerCapita: 1740,
-        lifeSatisfaction: 4.22,
-        country: "Afghanistan",
-      },
-      {
-        gdpPerCapita: 8174,
-        lifeSatisfaction: 4.33,
-        country: "Armenia",
-      },
-      {
-        gdpPerCapita: 15994,
-        lifeSatisfaction: 5.3,
-        country: "Azerbaijan",
-      },
-      {
-        gdpPerCapita: 3319,
-        lifeSatisfaction: 4.56,
-        country: "Bangladesh",
-      },
-      {
-        gdpPerCapita: 3463,
-        lifeSatisfaction: 4.46,
-        country: "Cambodia",
-      },
-      {
-        gdpPerCapita: 14401,
-        lifeSatisfaction: 5.32,
-        country: "China",
-      },
-      {
-        gdpPerCapita: 54279,
-        lifeSatisfaction: 5.5,
-        country: "Hong Kong",
-      },
-      {
-        gdpPerCapita: 6093,
-        lifeSatisfaction: 4.18,
-        country: "India",
-      },
-      {
-        gdpPerCapita: 10765,
-        lifeSatisfaction: 5.14,
-        country: "Indonesia",
-      },
-      {
-        gdpPerCapita: 16087,
-        lifeSatisfaction: 4.41,
-        country: "Iraq",
-      },
-      {
-        gdpPerCapita: 32613,
-        lifeSatisfaction: 7.16,
-        country: "Israel",
-      },
-      {
-        gdpPerCapita: 38240,
-        lifeSatisfaction: 5.95,
-        country: "Japan",
-      },
-      {
-        gdpPerCapita: 8390,
-        lifeSatisfaction: 5.27,
-        country: "Jordan",
-      },
-      {
-        gdpPerCapita: 23420,
-        lifeSatisfaction: 5.53,
-        country: "Kazakhstan",
-      },
-      {
-        gdpPerCapita: 3292,
-        lifeSatisfaction: 4.86,
-        country: "Kyrgyzstan",
-      },
-      {
-        gdpPerCapita: 12974,
-        lifeSatisfaction: 5.27,
-        country: "Lebanon",
-      },
-      {
-        gdpPerCapita: 11328,
-        lifeSatisfaction: 5.06,
-        country: "Mongolia",
-      },
-      {
-        gdpPerCapita: 5352,
-        lifeSatisfaction: 4.62,
-        country: "Myanmar",
-      },
-      {
-        gdpPerCapita: 2288,
-        lifeSatisfaction: 5.1,
-        country: "Nepal",
-      },
-      {
-        gdpPerCapita: 4866,
-        lifeSatisfaction: 5.55,
-        country: "Pakistan",
-      },
-      {
-        gdpPerCapita: 2685,
-        lifeSatisfaction: 4.91,
-        country: "Palestine",
-      },
-      {
-        gdpPerCapita: 7236,
-        lifeSatisfaction: 5.43,
-        country: "Philippines",
-      },
-      {
-        gdpPerCapita: 50458,
-        lifeSatisfaction: 6.47,
-        country: "Saudi Arabia",
-      },
-      {
-        gdpPerCapita: 81443,
-        lifeSatisfaction: 6.03,
-        country: "Singapore",
-      },
-      {
-        gdpPerCapita: 34986,
-        lifeSatisfaction: 5.97,
-        country: "South Korea",
-      },
-      {
-        gdpPerCapita: 2763,
-        lifeSatisfaction: 5.1,
-        country: "Tajikistan",
-      },
-      {
-        gdpPerCapita: 15682,
-        lifeSatisfaction: 6.07,
-        country: "Thailand",
-      },
-      {
-        gdpPerCapita: 23679,
-        lifeSatisfaction: 5.33,
-        country: "Turkey",
-      },
-      {
-        gdpPerCapita: 15648,
-        lifeSatisfaction: 5.89,
-        country: "Turkmenistan",
-      },
-      {
-        gdpPerCapita: 67133,
-        lifeSatisfaction: 6.83,
-        country: "United Arab Emirates",
-      },
-      {
-        gdpPerCapita: 6039,
-        lifeSatisfaction: 5.89,
-        country: "Uzbekistan",
-      },
-      {
-        gdpPerCapita: 5955,
-        lifeSatisfaction: 5.06,
-        country: "Vietnam",
-      },
-      {
-        gdpPerCapita: 2325,
-        lifeSatisfaction: 3.83,
-        country: "Yemen",
-      },
-    ],
-    Europe: [
-      {
-        gdpPerCapita: 11425,
-        lifeSatisfaction: 4.51,
-        country: "Albania",
-      },
-      {
-        gdpPerCapita: 44144,
-        lifeSatisfaction: 7.05,
-        country: "Austria",
-      },
-      {
-        gdpPerCapita: 16742,
-        lifeSatisfaction: 5.18,
-        country: "Belarus",
-      },
-      {
-        gdpPerCapita: 41946,
-        lifeSatisfaction: 6.95,
-        country: "Belgium",
-      },
-      {
-        gdpPerCapita: 11179,
-        lifeSatisfaction: 5.18,
-        country: "Bosnia and Herzegovina",
-      },
-      {
-        gdpPerCapita: 17709,
-        lifeSatisfaction: 4.84,
-        country: "Bulgaria",
-      },
-      {
-        gdpPerCapita: 31196,
-        lifeSatisfaction: 5.79,
-        country: "Cyprus",
-      },
-      {
-        gdpPerCapita: 31072,
-        lifeSatisfaction: 6.74,
-        country: "Czech Republic",
-      },
-      {
-        gdpPerCapita: 45686,
-        lifeSatisfaction: 7.56,
-        country: "Denmark",
-      },
-      {
-        gdpPerCapita: 27735,
-        lifeSatisfaction: 5.65,
-        country: "Estonia",
-      },
-      {
-        gdpPerCapita: 39423,
-        lifeSatisfaction: 7.66,
-        country: "Finland",
-      },
-      {
-        gdpPerCapita: 38059,
-        lifeSatisfaction: 6.48,
-        country: "France",
-      },
-      {
-        gdpPerCapita: 9267,
-        lifeSatisfaction: 4.45,
-        country: "Georgia",
-      },
-      {
-        gdpPerCapita: 44072,
-        lifeSatisfaction: 6.87,
-        country: "Germany",
-      },
-      {
-        gdpPerCapita: 24264,
-        lifeSatisfaction: 5.3,
-        country: "Greece",
-      },
-      {
-        gdpPerCapita: 25381,
-        lifeSatisfaction: 5.45,
-        country: "Hungary",
-      },
-      {
-        gdpPerCapita: 45276,
-        lifeSatisfaction: 7.51,
-        country: "Iceland",
-      },
-      {
-        gdpPerCapita: 62828,
-        lifeSatisfaction: 7.04,
-        country: "Ireland",
-      },
-      {
-        gdpPerCapita: 34620,
-        lifeSatisfaction: 5.95,
-        country: "Italy",
-      },
-      {
-        gdpPerCapita: 9332,
-        lifeSatisfaction: 5.76,
-        country: "Kosovo",
-      },
-      {
-        gdpPerCapita: 23712,
-        lifeSatisfaction: 5.94,
-        country: "Latvia",
-      },
-      {
-        gdpPerCapita: 27904,
-        lifeSatisfaction: 5.87,
-        country: "Lithuania",
-      },
-      {
-        gdpPerCapita: 97019,
-        lifeSatisfaction: 6.97,
-        country: "Luxembourg",
-      },
-      {
-        gdpPerCapita: 13055,
-        lifeSatisfaction: 5.35,
-        country: "Macedonia",
-      },
-      {
-        gdpPerCapita: 35694,
-        lifeSatisfaction: 6.59,
-        country: "Malta",
-      },
-      {
-        gdpPerCapita: 4944,
-        lifeSatisfaction: 5.58,
-        country: "Moldova",
-      },
-      {
-        gdpPerCapita: 15658,
-        lifeSatisfaction: 5.3,
-        country: "Montenegro",
-      },
-      {
-        gdpPerCapita: 47128,
-        lifeSatisfaction: 7.54,
-        country: "Netherlands",
-      },
-      {
-        gdpPerCapita: 63811,
-        lifeSatisfaction: 7.6,
-        country: "Norway",
-      },
-      {
-        gdpPerCapita: 26003,
-        lifeSatisfaction: 6.16,
-        country: "Poland",
-      },
-      {
-        gdpPerCapita: 13720,
-        lifeSatisfaction: 5.75,
-        country: "Serbia",
-      },
-      {
-        gdpPerCapita: 29156,
-        lifeSatisfaction: 5.99,
-        country: "Slovakia",
-      },
-      {
-        gdpPerCapita: 29803,
-        lifeSatisfaction: 5.94,
-        country: "Slovenia",
-      },
-      {
-        gdpPerCapita: 33261,
-        lifeSatisfaction: 6.32,
-        country: "Spain",
-      },
-      {
-        gdpPerCapita: 46441,
-        lifeSatisfaction: 7.37,
-        country: "Sweden",
-      },
-      {
-        gdpPerCapita: 56625,
-        lifeSatisfaction: 7.46,
-        country: "Switzerland",
-      },
-      {
-        gdpPerCapita: 7668,
-        lifeSatisfaction: 4.03,
-        country: "Ukraine",
-      },
-      {
-        gdpPerCapita: 38901,
-        lifeSatisfaction: 6.82,
-        country: "United Kingdom",
-      },
-    ],
-    Africa: [
-      {
-        gdpPerCapita: 13975,
-        lifeSatisfaction: 5.39,
-        country: "Algeria",
-      },
-      {
-        gdpPerCapita: 2010,
-        lifeSatisfaction: 4.01,
-        country: "Benin",
-      },
-      {
-        gdpPerCapita: 15513,
-        lifeSatisfaction: 3.5,
-        country: "Botswana",
-      },
-      {
-        gdpPerCapita: 1595,
-        lifeSatisfaction: 4.21,
-        country: "Burkina Faso",
-      },
-      {
-        gdpPerCapita: 3046,
-        lifeSatisfaction: 4.82,
-        country: "Cameroon",
-      },
-      {
-        gdpPerCapita: 648,
-        lifeSatisfaction: 2.69,
-        country: "Central African Republic",
-      },
-      {
-        gdpPerCapita: 1846,
-        lifeSatisfaction: 4.03,
-        country: "Chad",
-      },
-      {
-        gdpPerCapita: 5301,
-        lifeSatisfaction: 4.12,
-        country: "Congo",
-      },
-      {
-        gdpPerCapita: 3448,
-        lifeSatisfaction: 4.54,
-        country: "Cote d'Ivoire",
-      },
-      {
-        gdpPerCapita: 742,
-        lifeSatisfaction: 4.52,
-        country: "Democratic Republic of Congo",
-      },
-      {
-        gdpPerCapita: 10319,
-        lifeSatisfaction: 4.56,
-        country: "Egypt",
-      },
-      {
-        gdpPerCapita: 1608,
-        lifeSatisfaction: 4.3,
-        country: "Ethiopia",
-      },
-      {
-        gdpPerCapita: 16786,
-        lifeSatisfaction: 4.83,
-        country: "Gabon",
-      },
-      {
-        gdpPerCapita: 3980,
-        lifeSatisfaction: 4.51,
-        country: "Ghana",
-      },
-      {
-        gdpPerCapita: 1215,
-        lifeSatisfaction: 3.6,
-        country: "Guinea",
-      },
-      {
-        gdpPerCapita: 2926,
-        lifeSatisfaction: 4.4,
-        country: "Kenya",
-      },
-      {
-        gdpPerCapita: 2808,
-        lifeSatisfaction: 3.81,
-        country: "Lesotho",
-      },
-      {
-        gdpPerCapita: 754,
-        lifeSatisfaction: 3.35,
-        country: "Liberia",
-      },
-      {
-        gdpPerCapita: 1396,
-        lifeSatisfaction: 3.66,
-        country: "Madagascar",
-      },
-      {
-        gdpPerCapita: 1084,
-        lifeSatisfaction: 3.48,
-        country: "Malawi",
-      },
-      {
-        gdpPerCapita: 1963,
-        lifeSatisfaction: 4.02,
-        country: "Mali",
-      },
-      {
-        gdpPerCapita: 3572,
-        lifeSatisfaction: 4.47,
-        country: "Mauritania",
-      },
-      {
-        gdpPerCapita: 19549,
-        lifeSatisfaction: 5.61,
-        country: "Mauritius",
-      },
-      {
-        gdpPerCapita: 7266,
-        lifeSatisfaction: 5.39,
-        country: "Morocco",
-      },
-      {
-        gdpPerCapita: 907,
-        lifeSatisfaction: 4.23,
-        country: "Niger",
-      },
-      {
-        gdpPerCapita: 5439,
-        lifeSatisfaction: 5.22,
-        country: "Nigeria",
-      },
-      {
-        gdpPerCapita: 2380,
-        lifeSatisfaction: 4.59,
-        country: "Senegal",
-      },
-      {
-        gdpPerCapita: 1366,
-        lifeSatisfaction: 4.73,
-        country: "Sierra Leone",
-      },
-      {
-        gdpPerCapita: 12260,
-        lifeSatisfaction: 4.77,
-        country: "South Africa",
-      },
-      {
-        gdpPerCapita: 2583,
-        lifeSatisfaction: 2.9,
-        country: "Tanzania",
-      },
-      {
-        gdpPerCapita: 1382,
-        lifeSatisfaction: 3.88,
-        country: "Togo",
-      },
-      {
-        gdpPerCapita: 10752,
-        lifeSatisfaction: 4.52,
-        country: "Tunisia",
-      },
-      {
-        gdpPerCapita: 1714,
-        lifeSatisfaction: 4.23,
-        country: "Uganda",
-      },
-      {
-        gdpPerCapita: 3636,
-        lifeSatisfaction: 4.35,
-        country: "Zambia",
-      },
-      {
-        gdpPerCapita: 1860,
-        lifeSatisfaction: 3.74,
-        country: "Zimbabwe",
-      },
-    ],
-    "South America": [
-      {
-        gdpPerCapita: 18479,
-        lifeSatisfaction: 6.43,
-        country: "Argentina",
-      },
-      {
-        gdpPerCapita: 14024,
-        lifeSatisfaction: 6.37,
-        country: "Brazil",
-      },
-      {
-        gdpPerCapita: 6708,
-        lifeSatisfaction: 5.77,
-        country: "Bolivia",
-      },
-      {
-        gdpPerCapita: 22707,
-        lifeSatisfaction: 6.58,
-        country: "Chile",
-      },
-      {
-        gdpPerCapita: 13124,
-        lifeSatisfaction: 6.23,
-        country: "Colombia",
-      },
-      {
-        gdpPerCapita: 10462,
-        lifeSatisfaction: 6.12,
-        country: "Ecuador",
-      },
-      {
-        gdpPerCapita: 8878,
-        lifeSatisfaction: 5.8,
-        country: "Paraguay",
-      },
-      {
-        gdpPerCapita: 12072,
-        lifeSatisfaction: 5.7,
-        country: "Peru",
-      },
-      {
-        gdpPerCapita: 20047,
-        lifeSatisfaction: 6.17,
-        country: "Uruguay",
-      },
-    ],
-    Oceania: [
-      {
-        gdpPerCapita: 44414,
-        lifeSatisfaction: 7.25,
-        country: "Australia",
-      },
-      {
-        gdpPerCapita: 35269,
-        lifeSatisfaction: 7.23,
-        country: "New Zealand",
-      },
-    ],
-    "North America": [
-      {
-        gdpPerCapita: 43088,
-        lifeSatisfaction: 7.24,
-        country: "Canada",
-      },
-      {
-        gdpPerCapita: 15401,
-        lifeSatisfaction: 7.14,
-        country: "Costa Rica",
-      },
-      {
-        gdpPerCapita: 14099,
-        lifeSatisfaction: 5.24,
-        country: "Dominican Republic",
-      },
-      {
-        gdpPerCapita: 7990,
-        lifeSatisfaction: 6.14,
-        country: "El Salvador",
-      },
-      {
-        gdpPerCapita: 7367,
-        lifeSatisfaction: 6.36,
-        country: "Guatemala",
-      },
-      {
-        gdpPerCapita: 1654,
-        lifeSatisfaction: 3.35,
-        country: "Haiti",
-      },
-      {
-        gdpPerCapita: 4392,
-        lifeSatisfaction: 5.65,
-        country: "Honduras",
-      },
-      {
-        gdpPerCapita: 16831,
-        lifeSatisfaction: 6.82,
-        country: "Mexico",
-      },
-      {
-        gdpPerCapita: 5137,
-        lifeSatisfaction: 6.01,
-        country: "Nicaragua",
-      },
-      {
-        gdpPerCapita: 21335,
-        lifeSatisfaction: 6.12,
-        country: "Panama",
-      },
-      {
-        gdpPerCapita: 53273,
-        lifeSatisfaction: 6.8,
-        country: "United States",
-      },
-    ],
+    alabama_cs: [
+        { likes: 10, follows: 20 },
+        { likes: 14, follows: 25 },
+        { likes: 19, follows: 29 },
+        { likes: 23, follows: 35 },
+        { likes: 30, follows: 40 },
+        { likes: 34, follows: 46 },
+        { likes: 41, follows: 50 },
+        { likes: 45, follows: 56 },
+        { likes: 50, follows: 60 },
+        { likes: 55, follows: 66 },
+        { likes: 60, follows: 70 },
+        { likes: 66, follows: 76 },
+        { likes: 70, follows: 82 },
+        { likes: 76, follows: 87 },
+        { likes: 82, follows: 93 },
+        { likes: 88, follows: 98 },
+        { likes: 93, follows: 104 },
+        { likes: 97, follows: 110 },
+        { likes: 103, follows: 116 },
+        { likes: 108, follows: 122 },
+      ],
+      auburnidk: [
+        { likes: 15, follows: 25 },
+        { likes: 21, follows: 32 },
+        { likes: 28, follows: 39 },
+        { likes: 35, follows: 47 },
+        { likes: 41, follows: 53 },
+        { likes: 48, follows: 61 },
+        { likes: 54, follows: 68 },
+        { likes: 61, follows: 76 },
+        { likes: 67, follows: 83 },
+        { likes: 74, follows: 91 },
+        { likes: 80, follows: 98 },
+        { likes: 87, follows: 106 },
+        { likes: 93, follows: 113 },
+        { likes: 100, follows: 121 },
+        { likes: 107, follows: 128 },
+        { likes: 113, follows: 136 },
+        { likes: 120, follows: 143 },
+        { likes: 126, follows: 150 },
+        { likes: 133, follows: 157 },
+        { likes: 140, follows: 165 },
+      ],
+    newSchool1: Array.from({length: 20}, (_, i) => ({ likes: 12 + 10 * i, follows: 22 + 10 * i})),
+    newSchool2: Array.from({length: 20}, (_, i) => ({ likes: 18 + 10 * i, follows: 28 + 10 * i})),
+    newSchool3: Array.from({length: 20}, (_, i) => ({ likes: 11 + 10 * i, follows: 21 + 10 * i})),
+    newSchool4: Array.from({length: 20}, (_, i) => ({ likes: 10 + 10 * i, follows: 25 + 10 * i})),
+    newSchool5: Array.from({length: 20}, (_, i) => ({ likes: 17 + 10 * i, follows: 27 + 10 * i})),
+    newSchool6: Array.from({length: 20}, (_, i) => ({ likes: 5 + 10 * i, follows: 15 + 10 * i})),
+    newSchool7: Array.from({length: 20}, (_, i) => ({ likes: 8 + 10 * i, follows: 18 + 10 * i})),
+    
   };
 }
 
@@ -684,18 +84,18 @@ export class Chart6 {
     text: "Follower Count vs Likes (Scatter/large)",
   },
   theme: this.chartTheme,
-  series: Object.entries(getData()).map(([continent, data]) => ({
+  series: Object.entries(getData()).map(([username, data]) => ({
     data,
     type: "scatter",
-    xKey: "gdpPerCapita",
-    xName: "GDP Per Capita",
-    yKey: "lifeSatisfaction",
-    yName: continent,
-    labelKey: "country",
-    labelName: "Country",
-    label: {},
+    xKey: "likes",
+    xName: "Likes",
+    yKey: "follows",
+    yName: "Follows",
+    title: username,
+
+    // TODO: Trevor FIX THIS
     marker: {
-      size: 5,
+      size: 10,
     },
   })),
   axes: [
@@ -703,11 +103,8 @@ export class Chart6 {
       position: "bottom",
       type: "number",
       nice: false,
-      label: {
-        formatter: ({ value }) => `${value / 1000}K`,
-      },
       title: {
-        text: "National Income",
+        text: "Likes",
       },
       crosshair: {
         label: {
@@ -721,7 +118,7 @@ export class Chart6 {
       type: "number",
       nice: false,
       title: {
-        text: "Happiness",
+        text: "Follows",
       },
     },
   ],
@@ -729,7 +126,7 @@ export class Chart6 {
     position: "right",
     item: {
       marker: {
-        size: 5,
+        size: 10,
       },
     },
   },
