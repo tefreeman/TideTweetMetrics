0ebbdd78a5d46ecb2f0a42af6513aa3f0a06c912
EJYohannan <99680407+EJYohannan@users.noreply.github.com>
2024-04-22 17:45:11 -0500

    Documentation: Core -guards -interfaces modules/materials

--- web/frontend/src/core/interfaces/profile-interface.ts
+++ web/frontend/src/core/interfaces/profile-interface.ts
@@ -1,5 +1,8 @@
 import { I_PageMap } from './pages-interface';
 
+/**
+ * Represents a user profile.
+ */
 export interface I_Profile {
   id?: string;
   defaultCollege: string;
@@ -7,11 +10,17 @@ export interface I_Profile {
   doWantEmailReports: boolean;
 }
 
+/**
+ * Represents a file version.
+ */
 export interface I_FileVersion {
   version: string;
   id: string;
 }
 
+/**
+ * Represents a user and their role.
+ */
 export interface I_UserAndRole {
   uid: string;
   email: string;
