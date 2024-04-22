fef0e8ccf8f91af09958a2889241fd39f0ed8502
Trevor Freeman <tefreeman@crimson.ua.edu>
2024-04-22 00:42:30 -0500

    backend api updates and cloud functions

--- backend/middleware/middleware/urls.py
+++ backend/middleware/middleware/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for api project.
+URL configuration for middleware project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("metrics/", include("metrics.urls")),
+    path("polls/", include("api.urls")),
     path("admin/", admin.site.urls),
 ]
