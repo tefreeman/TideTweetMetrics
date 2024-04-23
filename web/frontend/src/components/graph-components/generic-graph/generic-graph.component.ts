275c1624210f8ec15573d4e702be4018f07be220
EJYohannan <99680407+EJYohannan@users.noreply.github.com>
2024-04-23 15:43:06 -0500

    missed this one for documentation

--- web/frontend/src/components/graph-components/generic-graph/generic-graph.component.ts
+++ web/frontend/src/components/graph-components/generic-graph/generic-graph.component.ts
@@ -13,6 +13,9 @@ import { AgChartOptions } from 'ag-charts-community';
 import { T_DisplayableGraph } from '../../../core/interfaces/displayable-data-interface';
 import { GraphMakerService } from '../../../core/services/graph-maker.service';
 
+/**
+ * Represents the GenericGraphComponent.
+ */
 @Component({
   selector: 'app-generic-graph',
   standalone: true,
@@ -22,23 +25,45 @@ import { GraphMakerService } from '../../../core/services/graph-maker.service';
   changeDetection: ChangeDetectionStrategy.OnPush, // Use onPush change detection strategy
 })
 export class GenericGraphComponent {
+  /**
+   * The displayable data for the graph.
+   */
   @Input({ required: true }) displayableData!: T_DisplayableGraph;
+
+  /**
+   * The height of the graph.
+   */
   @Input() height: string = '';
+
+  /**
+   * The reference to the graph container element.
+   */
   @ViewChild('graphContainer') graphContainer!: ElementRef;
 
   private graphMakerService: GraphMakerService = inject(GraphMakerService);
   private resizeObserver?: ResizeObserver;
   private debounceTimeout?: any; // Store the timeout to use across calls
+
+  /**
+   * The chart options for the graph.
+   */
   public chartOptions: AgChartOptions = {};
 
-  constructor(private cdr: ChangeDetectorRef) {}
+  constructor(private cdr: ChangeDetectorRef) { }
 
+  /**
+   * Initializes the component.
+   */
   ngOnInit(): void {
     this.chartOptions = this.graphMakerService.createChart(
       this.displayableData
     );
     console.log(this.chartOptions);
   }
+
+  /**
+   * Performs additional initialization after the view has been initialized.
+   */
   ngAfterViewInit(): void {
     this.adjustChartHeight();
     const resizeObserver = new ResizeObserver((entries) => {
@@ -50,6 +75,9 @@ export class GenericGraphComponent {
     this.resizeObserver = resizeObserver;
   }
 
+  /**
+   * Adjusts the height of the chart based on the width of the graph container.
+   */
   adjustChartHeight(): void {
     const width = this.graphContainer.nativeElement.offsetWidth;
     const newHeight = this.calculateHeightFromWidth(width);
@@ -58,11 +86,19 @@ export class GenericGraphComponent {
     this.cdr.detectChanges();
   }
 
+  /**
+   * Calculates the height of the chart based on the width of the graph container.
+   * @param width - The width of the graph container.
+   * @returns The calculated height of the chart.
+   */
   private calculateHeightFromWidth(width: number): number {
     // Implement your logic to calculate height from width. This is just an example.
     return width / (16 / 9); // Example for a 16:9 aspect ratio
   }
 
+  /**
+   * Cleans up resources before the component is destroyed.
+   */
   ngOnDestroy(): void {
     if (this.resizeObserver) {
       this.resizeObserver.disconnect();
@@ -71,6 +107,11 @@ export class GenericGraphComponent {
     clearTimeout(this.debounceTimeout);
   }
 
+  /**
+   * Executes a function after a specified delay.
+   * @param func - The function to execute.
+   * @param wait - The delay in milliseconds.
+   */
   private debounce(func: () => void, wait: number): void {
     clearTimeout(this.debounceTimeout);
     this.debounceTimeout = setTimeout(func, wait);
