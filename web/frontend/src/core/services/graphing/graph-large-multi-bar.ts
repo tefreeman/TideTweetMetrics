82e70a311180696c6cc8c6085cba0f78c86b79f0
Travis <travispnguyen@gmail.com>
2024-04-23 14:00:42 -0500

    Remove Titles

--- web/frontend/src/core/services/graphing/graph-large-multi-bar.ts
+++ web/frontend/src/core/services/graphing/graph-large-multi-bar.ts
@@ -100,7 +100,7 @@ export class GraphLargeMutliBar extends BaseGraph {
 
       data: this.getData(data),
       title: {
-        text: 'Likes (Bar/Large)',
+        enabled: false,
       },
       series: this.getSeries(data),
       axes: [
