06801efcaf7c30496d486c2cc5d99c389f7f740e
Trevor Freeman <tefreeman@crimson.ua.edu>
2024-04-24 11:25:32 -0500

    interface commends bad

--- web/frontend/src/core/interfaces/displayable-interface.ts
+++ web/frontend/src/core/interfaces/displayable-interface.ts
@@ -1,17 +1,5 @@
 import { T_MetricValue } from './metrics-interface';
 
-/**
- * @module TideTweetMetrics
- * @description This module contains interfaces related to displayable data and metrics.
- */
-
-/**
- * Represents the value of a metric.
- */
-
-/**
- * Represents the string type of a displayable element.
- */
 export type T_DisplayableTypeString =
   | 'stat-value'
   | 'stat-trend'
@@ -28,76 +16,28 @@ export type T_DisplayableTypeString =
   | 'small-graph-bar-grouped'
   | 'graph-scatter';
 
-/**
-
- * Represents the type of owner.
- */
 export type T_OwnerType = 'all' | 'top' | 'bottom' | 'specific';
 
-/**
- * Represents the parameters for owners.
-
- */
 export interface I_OwnersParams {
-  /**
-   * The type of owner.
-   */
   type: T_OwnerType;
-  /**
-   * The count of owners.
-   */
   count?: number;
-  /**
-   * The list of owners.
-   */
   owners: string[];
 }
 
-/**
- * Represents a displayable request.
- */
-
 export interface I_DisplayableRequest {
-  /**
-   * The name of the statistic.
-   */
   stat_name: string;
-  /**
-   * The parameters for owners.
-   */
   ownersParams: I_OwnersParams;
-  /**
-   * The type of displayable element.
-   */
   type: T_DisplayableTypeString;
-  /**
-   * If the displayable request is part of a group, the group ID.
-   */
   groupId?: string;
 
   metric_names?: string[];
 }
 
-/**
- * Represents displayable data.
- */
 export interface IDisplayableData extends I_DisplayableRequest {
-  /**
-   * The owners and their metric values.
-   */
   owners: { [owner: string]: T_MetricValue };
 }
 
-/**
- * Represents owner data.
- */
 export interface I_OwnerData {
-  /**
-   * The owner name.
-   */
   owner: string;
-  /**
-   * The metric value.
-   */
   value: T_MetricValue;
 }
--- web/frontend/src/core/interfaces/metrics-interface.ts
+++ web/frontend/src/core/interfaces/metrics-interface.ts
@@ -1,23 +1,11 @@
-/**
- * Represents a number or string value.
- */
-type NumberOrString = Number; // may need to change this to include astring later
+type NumberOrString = Number;
 
-/**
- * Represents a metric value that can be either a number, string, or an array of numbers or strings.
- */
 export type T_MetricValue = NumberOrString | Array<NumberOrString>;
 
-/**
- * Represents the owners of a metric and their corresponding metric values.
- */
 export interface I_MetricOwners {
   [owner: string]: T_MetricValue;
 }
 
-/**
- * Represents the metrics interface, which contains a collection of metric names and their corresponding metric owners.
- */
 export interface I_MetricsInterface {
   [stat_name: string]: I_MetricOwners;
 }
