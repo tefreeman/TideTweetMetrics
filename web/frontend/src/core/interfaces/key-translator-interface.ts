d9b1d13c65843e391bcf774febf5a024429361eb
Trevor Freeman <tefreeman@crimson.ua.edu>
2024-04-24 11:25:32 -0500

    interface commends bad

--- web/frontend/src/core/interfaces/key-translator-interface.ts
+++ web/frontend/src/core/interfaces/key-translator-interface.ts
@@ -1,23 +1,8 @@
-/**
- * Represents a key translator interface.
- */
 export interface I_KeyTranslator {
   [key: string]: {
-    /**
-     * The abbreviated version of the key.
-     */
     abr?: string;
-    /**
-     * The full version of the key.
-     */
     full: string;
-    /**
-     * The description of the key.
-     */
     desc?: string;
-    /**
-     * The order of the key.
-     */
     order: number;
   };
 }
