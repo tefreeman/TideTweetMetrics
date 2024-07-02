443e0e7bd7dd462db82fc452752c2adc24df6883
Trevor Freeman <tefreeman@crimson.ua.edu>
2024-07-01 19:15:46 -0500

    example optimizer data

--- src/frontend/app/src/views/registered/optimizer/optimizer.component.ts
+++ src/frontend/app/src/views/registered/optimizer/optimizer.component.ts
@@ -6,7 +6,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { HttpClient } from '@angular/common/http';
 import { NgIf } from '@angular/common';
-
+import { optimizerData } from './optimizer.example';
 @Component({
   selector: 'app-optimizer',
   standalone: true,
@@ -22,34 +22,9 @@ import { NgIf } from '@angular/common';
   styleUrls: ['./optimizer.component.css'],
 })
 export class OptimizerComponent implements OnInit {
-  value = '';
-  improvedTweet = ''; // To store the improved tweet
-  oldLikes = 0;
-  newLikes = 0;
-  isRunning = false;
   constructor(private http: HttpClient) {}
 
   ngOnInit() {}
 
-  submitTweet() {
-    this.isRunning = true;
-    const tweetData = { tweet: this.value };
-    this.http
-      .post<{ new_tweet: string; likes: number; improved_likes: number }>(
-        'http://73.58.28.154:5000/',
-        tweetData
-      )
-      .subscribe({
-        next: (response) => {
-          this.improvedTweet = response.new_tweet;
-          this.oldLikes = response.likes;
-          this.newLikes = response.improved_likes;
-          this.isRunning = false;
-        },
-        error: (error) => {
-          console.error('There was an error!', error);
-          this.isRunning = false;
-        },
-      });
-  }
+  submitTweet() {}
 }
---
+++ src/frontend/app/src/views/registered/optimizer/optimizer.example.ts
@@ -0,0 +1,136 @@
+export const optimizerData = {
+  tweet: {
+    text: 'Congratulations @Chris_Crawford_on becoming the rank of Associate Professor!',
+  },
+  prediction: 44.931949615478516,
+  children: [
+    {
+      tweet: {
+        text: 'Congratulations @Chris_Crawford_on becoming the rank of Associate Professor!',
+      },
+      prediction: 44.931949615478516,
+      children: [],
+    },
+    {
+      tweet: {
+        text: 'Congratulations to @Chris_Crawford_ on being promoted to Associate Professor! Your dedication and hard work inspire us all.',
+      },
+      prediction: 55.41691970825195,
+      children: [],
+    },
+    {
+      tweet: {
+        text: "We're thrilled to announce that @Chris_Crawford_ has achieved the rank of Associate Professor. Join us in celebrating this remarkable milestone!",
+      },
+      prediction: 60.37377166748047,
+      children: [],
+    },
+    {
+      tweet: {
+        text: 'A big round of applause for @Chris_Crawford_ on his promotion to Associate Professor! Your commitment to excellence is truly commendable.',
+      },
+      prediction: 58.57157897949219,
+      children: [],
+    },
+    {
+      tweet: {
+        text: 'Proud to share that @Chris_Crawford_ has earned the prestigious title of Associate Professor. Your achievements highlight the strength of our faculty.',
+      },
+      prediction: 61.984439849853516,
+      children: [
+        {
+          tweet: {
+            text: 'Proud to share that @Chris_Crawford_ has earned the prestigious title of Associate Professor. Your achievements highlight the strength of our faculty.',
+          },
+          prediction: 61.984439849853516,
+          children: [],
+        },
+        {
+          tweet: {
+            text: 'Join us in congratulating @Chris_Crawford_ on earning the distinguished title of Associate Professor! His accomplishments exemplify the excellence of our faculty.',
+          },
+          prediction: 64.61137390136719,
+          children: [],
+        },
+        {
+          tweet: {
+            text: "Thrilled to announce @Chris_Crawford_'s new title as Associate Professor! Celebrate his exceptional contributions to our academic community.",
+          },
+          prediction: 59.72990798950195,
+          children: [],
+        },
+        {
+          tweet: {
+            text: 'We are honored to recognize @Chris_Crawford_ as he receives the prestigious title of Associate Professor. A testament to the strength and dedication of our faculty.',
+          },
+          prediction: 65.26244354248047,
+          children: [],
+        },
+        {
+          tweet: {
+            text: "Congratulations to @Chris_Crawford_ on being named Associate Professor! His achievements continue to elevate our institution's academic excellence.",
+          },
+          prediction: 61.155216217041016,
+          children: [],
+        },
+        {
+          tweet: {
+            text: "Proudly celebrating @Chris_Crawford_'s promotion to Associate Professor. His remarkable journey underscores the outstanding quality of our faculty.",
+          },
+          prediction: 57.09855651855469,
+          children: [],
+        },
+      ],
+    },
+    {
+      tweet: {
+        text: 'Join us in congratulating @Chris_Crawford_ on his well-deserved promotion to Associate Professor. Your contributions to academia are invaluable!',
+      },
+      prediction: 60.410648345947266,
+      children: [
+        {
+          tweet: {
+            text: 'Join us in congratulating @Chris_Crawford_ on his well-deserved promotion to Associate Professor. Your contributions to academia are invaluable!',
+          },
+          prediction: 60.410648345947266,
+          children: [],
+        },
+        {
+          tweet: {
+            text: "Join us in celebrating @Chris_Crawford_'s promotion to Associate Professor. His dedication and academic contributions are truly inspiring!",
+          },
+          prediction: 56.757667541503906,
+          children: [],
+        },
+        {
+          tweet: {
+            text: "We are thrilled to announce the promotion of @Chris_Crawford_ to Associate Professor! Let's applaud his outstanding achievements in academia.",
+          },
+          prediction: 59.93671417236328,
+          children: [],
+        },
+        {
+          tweet: {
+            text: 'Congratulations to @Chris_Crawford_ on his promotion to Associate Professor. His hard work and dedication to research and teaching are commendable. #AcademicExcellence',
+          },
+          prediction: 65.66936492919922,
+          children: [],
+        },
+        {
+          tweet: {
+            text: 'Hats off to @Chris_Crawford_ for being promoted to Associate Professor! His invaluable contributions to our University pave the way for future innovation.',
+          },
+          prediction: 60.96601486206055,
+          children: [],
+        },
+        {
+          tweet: {
+            text: "It's official! @Chris_Crawford_ is now an Associate Professor. Join us in recognizing his exceptional commitment and contributions to academic excellence.",
+          },
+          prediction: 62.30861282348633,
+          children: [],
+        },
+      ],
+    },
+  ],
+};
